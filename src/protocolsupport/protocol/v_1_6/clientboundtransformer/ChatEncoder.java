cf43b5294b30b206b172504954a83ec4f77ecd1f
Shevchik <Shev4ik.den@gmail.com>
2014-12-19 23:41:33 +0400

    Forgot that file

---
+++ src/protocolsupport/protocol/v_1_6/clientboundtransformer/ChatEncoder.java
@@ -0,0 +1,17 @@
+package protocolsupport.protocol.v_1_6.clientboundtransformer;
+
+import org.bukkit.craftbukkit.libs.com.google.gson.Gson;
+import org.bukkit.craftbukkit.libs.com.google.gson.GsonBuilder;
+import org.bukkit.craftbukkit.libs.com.google.gson.JsonObject;
+
+public class ChatEncoder {
+
+	private static final Gson gson = new GsonBuilder().create();
+
+	public static String encode(String message) {
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("text", message);
+		return gson.toJson(jsonObject);
+	}
+
+}
