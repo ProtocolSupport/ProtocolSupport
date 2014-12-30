b29131e2b5156292416fcc72e7920372ec6b8147
Shevchik <Shev4ik.den@gmail.com>
2014-12-30 11:41:04 +0400

    Add command to get entity by id for better debug

--- src/protocolsupport/commands/CommandHandler.java
+++ src/protocolsupport/commands/CommandHandler.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.craftbukkit.v1_8_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 import protocolsupport.protocol.DataStorage;
@@ -25,6 +26,10 @@ public class CommandHandler implements CommandExecutor {
 			sender.sendMessage(ChatColor.GOLD+"[1.6.4]: "+getPlayersStringForProtocol(ProtocolVersion.MINECRAFT_1_6_4));
 			sender.sendMessage(ChatColor.GOLD+"[1.6.2]: "+getPlayersStringForProtocol(ProtocolVersion.MINECRAFT_1_6_2));
 			sender.sendMessage(ChatColor.GOLD+"[1.5.2]: "+getPlayersStringForProtocol(ProtocolVersion.MINECRAFT_1_5_2));
+			return true;
+		}
+		if (args.length == 2 && args[0].equalsIgnoreCase("entity")) {
+			sender.sendMessage(ChatColor.GOLD+"Entity: "+CraftPlayer.class.cast(((Player) sender)).getHandle().world.a(Integer.parseInt(args[1])).toString());
 			return true;
 		}
 		return false;
