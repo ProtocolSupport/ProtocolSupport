1198faf5ab7b54ae0cec01439486936a2ed84212
Leonardo <leonardomalaman@gmail.com>
2015-11-01 18:28:27 -0200

    Remap all buttons (77, 143) to sign (63)
    I don't know a better replacer for those blocks

--- src/protocolsupport/protocol/typeremapper/id/IdRemapper.java
+++ src/protocolsupport/protocol/typeremapper/id/IdRemapper.java
@@ -106,6 +106,9 @@ public class IdRemapper {
 			registerRemapEntry(197, 64, ProtocolVersionsHelper.BEFORE_MINECRAFT_PE_0_13);
 			// standing banner -> standing sign
 			registerRemapEntry(176, 63, ProtocolVersionsHelper.BEFORE_MINECRAFT_PE_0_13);
+			// all buttons -> sign (well, I don't know a better replacer for this)
+			registerRemapEntry(77, 63, ProtocolVersionsHelper.BEFORE_MINECRAFT_PE_0_13);
+			registerRemapEntry(143, 63, ProtocolVersionsHelper.BEFORE_MINECRAFT_PE_0_13);
 		}
 		@Override
 		protected RemappingTable createTable() {
