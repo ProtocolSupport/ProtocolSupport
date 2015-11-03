175e443c7b3c3421861fffc0772e8cb05ed3b628
Leonardo <leonardomalaman@gmail.com>
2015-11-03 15:15:23 -0200

    Remap lever (69) to sign (63) (well, I don't know a better replacer for this)

--- src/protocolsupport/protocol/typeremapper/id/IdRemapper.java
+++ src/protocolsupport/protocol/typeremapper/id/IdRemapper.java
@@ -106,7 +106,7 @@ public class IdRemapper {
 			registerRemapEntry(197, 64, ProtocolVersionsHelper.MCPE);
 			// standing banner -> standing sign
 			registerRemapEntry(176, 63, ProtocolVersionsHelper.MCPE);
-			// all buttons -	> sign (well, I don't know a better replacer for this)
+			// all buttons -> sign (well, I don't know a better replacer for this)
 			registerRemapEntry(77, 63, ProtocolVersionsHelper.MCPE);
 			registerRemapEntry(143, 63, ProtocolVersionsHelper.MCPE);
 			// (sticky) piston -> stone
@@ -120,6 +120,8 @@ public class IdRemapper {
 			registerRemapEntry(25, 5, ProtocolVersionsHelper.MCPE);
 			// detector rail -> powered rail
 			registerRemapEntry(28, 27, ProtocolVersionsHelper.MCPE);
+			// lever -> sign (well, I don't know a better replacer for this)
+			registerRemapEntry(69, 63, ProtocolVersionsHelper.MCPE);
 		}
 		@Override
 		protected RemappingTable createTable() {
