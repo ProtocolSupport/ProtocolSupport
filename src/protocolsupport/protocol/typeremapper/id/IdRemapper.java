1592ad0394c8c712d01a994bb3858e6f81028779
Leonardo <leonardomalaman@gmail.com>
2015-11-03 15:12:28 -0200

    Remap dispenser (23) to stone (1)

--- src/protocolsupport/protocol/typeremapper/id/IdRemapper.java
+++ src/protocolsupport/protocol/typeremapper/id/IdRemapper.java
@@ -114,6 +114,8 @@ public class IdRemapper {
 			registerRemapEntry(33, 1, ProtocolVersionsHelper.MCPE);
 			// piston head -> stone
 			registerRemapEntry(34, 1, ProtocolVersionsHelper.MCPE);
+			// dispenser -> stone
+			registerRemapEntry(23, 1, ProtocolVersionsHelper.MCPE);
 		}
 		@Override
 		protected RemappingTable createTable() {
