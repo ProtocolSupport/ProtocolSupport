aaeee91e8a309097bc7e3de36cd203a6a34bcc21
Shevchik <Shev4ik.den@gmail.com>
2015-12-23 00:14:32 +0300

    Optimize registry a bit

--- src/protocolsupport/protocol/transformer/utils/registry/MiddleTransformerRegistry.java
+++ src/protocolsupport/protocol/transformer/utils/registry/MiddleTransformerRegistry.java
@@ -1,6 +1,5 @@
 package protocolsupport.protocol.transformer.utils.registry;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.EnumMap;
 
 import net.minecraft.server.v1_8_R3.EnumProtocol;
@@ -8,16 +7,18 @@ import net.minecraft.server.v1_8_R3.EnumProtocol;
 public class MiddleTransformerRegistry<T> {
 
 	private final EnumMap<EnumProtocol, Object[]> registry = new EnumMap<>(EnumProtocol.class);
-
-	public void register(EnumProtocol protocol, int packetId, Class<? extends T> packetTransformer) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException {
-		if (!registry.containsKey(protocol)) {
+	{
+		for (EnumProtocol protocol : EnumProtocol.values()) {
 			registry.put(protocol, new Object[256]);
 		}
+	}
+
+	public void register(EnumProtocol protocol, int packetId, Class<? extends T> packetTransformer) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException {
 		registry.get(protocol)[packetId] = packetTransformer.newInstance();
 	}
 
 	@SuppressWarnings("unchecked")
-	public T getTransformer(EnumProtocol protocol, int packetId) throws InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public T getTransformer(EnumProtocol protocol, int packetId) {
 		Object[] transformers = registry.get(protocol);
 		if (transformers == null) {
 			return null;
