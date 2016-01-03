ce1f7e6967e44713da128569692d9db551931c6d
Shevchik <Shev4ik.den@gmail.com>
2016-01-03 20:09:03 +0300

    Move to separate class

--- src/protocolsupport/api/events/ServerPingResponseEvent.java
+++ src/protocolsupport/api/events/ServerPingResponseEvent.java
@@ -1,19 +1,9 @@
 package protocolsupport.api.events;
 
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.imageio.ImageIO;
-
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang3.Validate;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
@@ -113,22 +103,6 @@ public class ServerPingResponseEvent extends Event {
 
 	public static HandlerList getHandlerList() {
 		return list;
-	}
-
-	public static String loadIcon(File file) throws IOException {
-		return loadIcon(new FileInputStream(file));
-	}
-
-	public static String loadIcon(InputStream rawStream) throws IOException {
-		return loadIcon(ImageIO.read(rawStream));
-	}
-
-	public static String loadIcon(BufferedImage image) throws IOException {
-        Validate.isTrue(image.getWidth() == 64, "Must be 64 pixels wide");
-        Validate.isTrue(image.getHeight() == 64, "Must be 64 pixels high");
-		ByteArrayOutputStream data = new ByteArrayOutputStream();
-		ImageIO.write(image, "PNG", data);
-		return "data:image/png;base64," + Base64.encodeBase64String(data.toByteArray());
 	}
 
 }
