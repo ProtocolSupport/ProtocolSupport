6a027bda601e675be669807a5ef0cce8a642d380
Shevchik <Shev4ik.den@gmail.com>
2016-03-01 18:29:27 +0300

    Fake receive teleport confirm packet if position packet is sent with teleport id.

--- src/protocolsupport/protocol/transformer/middlepacket/clientbound/play/MiddlePosition.java
+++ src/protocolsupport/protocol/transformer/middlepacket/clientbound/play/MiddlePosition.java
@@ -1,9 +1,14 @@
 package protocolsupport.protocol.transformer.middlepacket.clientbound.play;
 
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_9_R1.entity.CraftPlayer;
+import org.spigotmc.SneakyThrow;
 
+import net.minecraft.server.v1_9_R1.CancelledPacketHandleException;
+import net.minecraft.server.v1_9_R1.PacketPlayInTeleportAccept;
 import protocolsupport.protocol.PacketDataSerializer;
 import protocolsupport.protocol.transformer.middlepacket.ClientBoundMiddlePacket;
+import protocolsupport.protocol.transformer.middlepacketimpl.PacketCreator;
 
 public abstract class MiddlePosition<T> extends ClientBoundMiddlePacket<T> {
 
@@ -43,8 +48,18 @@ public abstract class MiddlePosition<T> extends ClientBoundMiddlePacket<T> {
 		if ((field & 0x10) != 0) {
 			pitch += location.getPitch();
 		}
-		//TODO: fake receive teleport confirm packet for versions < 1.9
 		teleportConfirmId = serializer.readVarInt();
+		//fake a teleport confirm packet
+		if (teleportConfirmId != 0) {
+			PacketCreator creator = PacketCreator.create(new PacketPlayInTeleportAccept());
+			creator.writeVarInt(teleportConfirmId);
+			try {
+				((CraftPlayer) player).getHandle().playerConnection.a((PacketPlayInTeleportAccept) creator.create());
+			} catch (CancelledPacketHandleException e) {
+			} catch (Exception e) {
+				SneakyThrow.sneaky(e);
+			}
+		}
 	}
 
 }
