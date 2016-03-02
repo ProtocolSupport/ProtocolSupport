bffd6196906e4016c412ee39f3ff75e07727548e
Shevchik <Shev4ik.den@gmail.com>
2016-03-02 18:13:39 +0300

    Add new enchantments and effects to skipping table

--- src/protocolsupport/protocol/typeskipper/id/IdSkipper.java
+++ src/protocolsupport/protocol/typeskipper/id/IdSkipper.java
@@ -6,11 +6,13 @@ public class IdSkipper {
 
 	public static final SkippingRegistry ENCHANT = new SkippingRegistry() {
 		{
+			//frost walker, mending
+			registerSkipEntry(9, ProtocolVersionsHelper.BEFORE_1_9);
+			registerSkipEntry(70, ProtocolVersionsHelper.BEFORE_1_9);
 			//depth strider
 			registerSkipEntry(8, ProtocolVersionsHelper.BEFORE_1_8);
-			//lure
+			//lure, luck of the sea
 			registerSkipEntry(62, ProtocolVersionsHelper.BEFORE_1_7);
-			//luck of the sea
 			registerSkipEntry(61, ProtocolVersionsHelper.BEFORE_1_7);
 		}
 		@Override
@@ -21,11 +23,14 @@ public class IdSkipper {
 
 	public static final SkippingRegistry EFFECT = new SkippingRegistry() {
 		{
-			//health boost
+			//glowing, levitation, luck, unluck
+			registerSkipEntry(24, ProtocolVersionsHelper.BEFORE_1_9);
+			registerSkipEntry(25, ProtocolVersionsHelper.BEFORE_1_9);
+			registerSkipEntry(26, ProtocolVersionsHelper.BEFORE_1_9);
+			registerSkipEntry(27, ProtocolVersionsHelper.BEFORE_1_9);
+			//health boost, absorbtion, saturation
 			registerSkipEntry(21, ProtocolVersionsHelper.BEFORE_1_6);
-			//absorbtion
 			registerSkipEntry(22, ProtocolVersionsHelper.BEFORE_1_6);
-			//saturation
 			registerSkipEntry(23, ProtocolVersionsHelper.BEFORE_1_6);
 		}
 		@Override
