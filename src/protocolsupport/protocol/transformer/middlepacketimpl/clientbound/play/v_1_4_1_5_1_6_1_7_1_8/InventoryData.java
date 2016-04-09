febc30332fb8184d99ddbafc0c0ec749b9373994
Shevchik <Shev4ik.den@gmail.com>
2016-04-09 16:40:20 +0300

    Fix enchantment display in enchantment table

--- src/protocolsupport/protocol/transformer/middlepacketimpl/clientbound/play/v_1_4_1_5_1_6_1_7_1_8/InventoryData.java
+++ src/protocolsupport/protocol/transformer/middlepacketimpl/clientbound/play/v_1_4_1_5_1_6_1_7_1_8/InventoryData.java
@@ -14,10 +14,26 @@ import protocolsupport.utils.recyclable.RecyclableSingletonList;
 public class InventoryData extends MiddleInventoryData<RecyclableCollection<PacketData>> {
 
 	private static final int[] furTypeTr = {1, 2, 0};
+	private final int[] enchTypeVal = new int[10];
 
 	@Override
 	public RecyclableCollection<PacketData> toData(ProtocolVersion version) throws IOException {
-		if (version.isBefore(ProtocolVersion.MINECRAFT_1_8) && player.getOpenInventory().getType() == InventoryType.FURNACE) {
+		if (version == ProtocolVersion.MINECRAFT_1_8 && player.getOpenInventory().getType() == InventoryType.ENCHANTING) {
+			enchTypeVal[type] = value;
+			if (type >= 7 && type <= 9) {
+				PacketData serializer = PacketData.create(ClientBoundPacket.PLAY_WINDOW_DATA_ID, version);
+				serializer.writeByte(windowId);
+				serializer.writeShort(type - 3);
+				serializer.writeShort((value << 8) | enchTypeVal[type - 3]);
+				return RecyclableSingletonList.<PacketData>create(serializer);
+			} else if (type >= 4 && type <= 6) {
+				PacketData serializer = PacketData.create(ClientBoundPacket.PLAY_WINDOW_DATA_ID, version);
+				serializer.writeByte(windowId);
+				serializer.writeShort(type);
+				serializer.writeShort(((enchTypeVal[type + 3]) << 8) | value);
+				return RecyclableSingletonList.<PacketData>create(serializer);
+			}
+		} else if (version.isBefore(ProtocolVersion.MINECRAFT_1_8) && player.getOpenInventory().getType() == InventoryType.FURNACE) {
 			if (type < furTypeTr.length) {
 				type = furTypeTr[type];
 			}
