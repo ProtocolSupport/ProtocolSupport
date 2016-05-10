e724afd9022a5cf752923a32f1503c7a69c15cf4
Shevchik <Shev4ik.den@gmail.com>
2016-05-10 05:54:24 +0300

    Do not send particles with type > 41 for 1.8 versions

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_1_8/WorldParticle.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_1_8/WorldParticle.java
@@ -7,12 +7,16 @@ import protocolsupport.protocol.packet.ClientBoundPacket;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleWorldParticle;
 import protocolsupport.protocol.packet.middleimpl.PacketData;
 import protocolsupport.utils.recyclable.RecyclableCollection;
+import protocolsupport.utils.recyclable.RecyclableEmptyList;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
 public class WorldParticle extends MiddleWorldParticle<RecyclableCollection<PacketData>> {
 
 	@Override
 	public RecyclableCollection<PacketData> toData(ProtocolVersion version) throws IOException {
+		if (type > 41) {
+			return RecyclableEmptyList.get();
+		}
 		PacketData serializer = PacketData.create(ClientBoundPacket.PLAY_WORLD_PARTICLES_ID, version);
 		serializer.writeInt(type);
 		serializer.writeBoolean(longdist);
