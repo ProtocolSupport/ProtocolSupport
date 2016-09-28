210ecd50747a2f53004d42c7db976d66c220a1a8
Shevchik <Shev4ik.den@gmail.com>
2016-09-28 17:49:41 +0300

    Rename decoders in preparation for full 1.9 decoder implementation

---
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_1_8__1_9_r1__1_9_r2/Animation.java
@@ -0,0 +1,14 @@
+package protocolsupport.protocol.packet.middleimpl.serverbound.play.v_1_8__1_9_r1__1_9_r2;
+
+import java.io.IOException;
+
+import protocolsupport.protocol.packet.middle.serverbound.play.MiddleAnimation;
+import protocolsupport.protocol.serializer.ProtocolSupportPacketDataSerializer;
+
+public class Animation extends MiddleAnimation {
+
+	@Override
+	public void readFromClientData(ProtocolSupportPacketDataSerializer serializer) throws IOException {
+	}
+
+}
