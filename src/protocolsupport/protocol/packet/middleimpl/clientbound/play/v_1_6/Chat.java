9925c6e4b837667d7073452b11bb0667ff50fe4c
Shevchik <Shev4ik.den@gmail.com>
2016-09-29 19:26:33 +0300

    Move packet encoders/decoders to pipeline package

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_1_6/Chat.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_1_6/Chat.java
@@ -1,11 +1,14 @@
 package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_1_6;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.api.chat.ChatAPI;
 import protocolsupport.protocol.packet.ClientBoundPacket;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleChat;
 import protocolsupport.protocol.packet.middleimpl.PacketData;
-import protocolsupport.protocol.packet.v_1_6.utils.ChatEncoder;
 import protocolsupport.utils.recyclable.RecyclableCollection;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
@@ -14,8 +17,16 @@ public class Chat extends MiddleChat<RecyclableCollection<PacketData>> {
 	@Override
 	public RecyclableCollection<PacketData> toData(ProtocolVersion version) {
 		PacketData serializer = PacketData.create(ClientBoundPacket.PLAY_CHAT_ID, version);
-		serializer.writeString(ChatEncoder.encode(ChatAPI.fromJSON(chatJson).toLegacyText()));
+		serializer.writeString(encode(ChatAPI.fromJSON(chatJson).toLegacyText()));
 		return RecyclableSingletonList.create(serializer);
+	}
+
+	private static final Gson gson = new GsonBuilder().create();
+
+	private static String encode(String message) {
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("text", message);
+		return gson.toJson(jsonObject);
 	}
 
 }
