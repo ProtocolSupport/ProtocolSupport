26d8b294b9fdaf1a8351200f9adc5ae35e4a4f6c
Shevchik <Shev4ik.den@gmail.com>
2016-10-03 03:17:38 +0300

    Fix update sign decode

--- src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_1_8__1_9_r1__1_9_r2__1_10/UpdateSign.java
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_1_8__1_9_r1__1_9_r2__1_10/UpdateSign.java
@@ -1,23 +1,25 @@
 package protocolsupport.protocol.packet.middleimpl.serverbound.play.v_1_8__1_9_r1__1_9_r2__1_10;
 
-import java.io.IOException;
-
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import io.netty.handler.codec.DecoderException;
+import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.packet.middle.serverbound.play.MiddleUpdateSign;
 import protocolsupport.protocol.serializer.ProtocolSupportPacketDataSerializer;
 
 public class UpdateSign extends MiddleUpdateSign {
 
+	private final JSONParser parser = new JSONParser();
+
 	@Override
-	public void readFromClientData(ProtocolSupportPacketDataSerializer serializer) throws IOException {
+	public void readFromClientData(ProtocolSupportPacketDataSerializer serializer) {
 		position = serializer.readPosition();
 		for (int i = 0; i < 4; i++) {
 			try {
-				lines[i] = (String) new JSONParser().parse(serializer.readString());
+				lines[i] = (String) (serializer.getVersion().isAfter(ProtocolVersion.MINECRAFT_1_8) ? serializer.readString() : parser.parse(serializer.readString()));
 			} catch (ParseException e) {
-				throw new IOException(e);
+				throw new DecoderException(e);
 			}
 		}
 	}
