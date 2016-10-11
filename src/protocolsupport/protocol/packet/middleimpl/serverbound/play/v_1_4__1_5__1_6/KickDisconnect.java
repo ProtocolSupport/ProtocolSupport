2ce96719e0c34e5430bacb8524de2bae5261fcac
BeYkeRYkt <beykerykt@gmail.com>
2016-10-12 00:04:11 +0700

    Update #36

--- src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_1_4__1_5__1_6/KickDisconnect.java
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_1_4__1_5__1_6/KickDisconnect.java
@@ -1,5 +1,9 @@
 package protocolsupport.protocol.packet.middleimpl.serverbound.play.v_1_4__1_5__1_6;
 
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
+
+import net.minecraft.server.v1_10_R1.ChatComponentText;
+import net.minecraft.server.v1_10_R1.EntityPlayer;
 import protocolsupport.protocol.packet.middle.ServerBoundMiddlePacket;
 import protocolsupport.protocol.packet.middleimpl.PacketCreator;
 import protocolsupport.protocol.serializer.ProtocolSupportPacketDataSerializer;
@@ -10,7 +14,9 @@ public class KickDisconnect extends ServerBoundMiddlePacket {
 
 	@Override
 	public void readFromClientData(ProtocolSupportPacketDataSerializer serializer) {
-		serializer.readString();
+		// Based on: https://github.com/ProtocolSupport/ProtocolSupport/pull/36
+		EntityPlayer ep = ((CraftPlayer) player).getHandle();
+		ep.playerConnection.networkManager.close(new ChatComponentText(serializer.readString()));
 	}
 
 	@Override
