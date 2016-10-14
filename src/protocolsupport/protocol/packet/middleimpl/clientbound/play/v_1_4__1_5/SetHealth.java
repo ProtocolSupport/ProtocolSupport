21a6e48ad5863206ab6b20e57e50c175d33de9e7
EntryPoint <entrypoint_@naver.com>
2016-10-14 20:44:35 +0900

    Update 1.5.2 health display

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_1_4__1_5/SetHealth.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_1_4__1_5/SetHealth.java
@@ -11,9 +11,19 @@ import protocolsupport.utils.recyclable.RecyclableSingletonList;
 public class SetHealth extends MiddleSetHealth<RecyclableCollection<PacketData>> {
 
 	@Override
+	public boolean needsPlayer() {
+		return true;
+	}
+
+	@Override
 	public RecyclableCollection<PacketData> toData(ProtocolVersion version) {
 		PacketData serializer = PacketData.create(ClientBoundPacket.PLAY_UPDATE_HEALTH_ID, version);
-		serializer.writeShort(MathHelper.f(health));
+		Double maxHealth = player.getMaxHealth();
+		Integer hp = MathHelper.f(health);
+		if (maxHealth > 20) {
+			hp = MathHelper.f(health / (maxHealth / 20));
+		}
+		serializer.writeShort(hp);
 		serializer.writeShort(food);
 		serializer.writeFloat(saturation);
 		return RecyclableSingletonList.create(serializer);
