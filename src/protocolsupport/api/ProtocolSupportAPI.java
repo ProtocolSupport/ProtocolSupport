e87c78eeb681314a564c3d1ffec96e573f4847cf
Shevchik <Shev4ik.den@gmail.com>
2016-10-26 10:48:10 +0300

    If getting connection from player, try to get it from nms player first if possible. Also add an option to iterate over all connections and compare addresses to get needed on. Useful if you have any plugins that change player address.

--- src/protocolsupport/api/ProtocolSupportAPI.java
+++ src/protocolsupport/api/ProtocolSupportAPI.java
@@ -4,16 +4,24 @@ import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
+import net.minecraft.server.v1_10_R1.EntityPlayer;
 import protocolsupport.api.remapper.BlockRemapperControl;
 import protocolsupport.api.remapper.ItemRemapperControl;
+import protocolsupport.protocol.ConnectionImpl;
 import protocolsupport.protocol.storage.ProtocolStorage;
+import protocolsupport.utils.Utils;
+import protocolsupport.utils.Utils.Converter;
 
 public class ProtocolSupportAPI {
 
+	private static final boolean getconnectioncheckall = Utils.getJavaPropertyValue("api.getconnection.checkall", false, Converter.STRING_TO_BOOLEAN);
+
 	public static ProtocolVersion getProtocolVersion(Player player) {
-		return getProtocolVersion(player.getAddress());
+		Connection connection = getConnection(player);
+		return connection != null ? connection.getVersion() : ProtocolVersion.UNKNOWN;
 	}
 
 	public static ProtocolVersion getProtocolVersion(SocketAddress address) {
@@ -26,11 +34,28 @@ public class ProtocolSupportAPI {
 	}
 
 	public static Connection getConnection(Player player) {
+		if (player instanceof CraftPlayer) {
+			EntityPlayer nmsplayer = ((CraftPlayer) player).getHandle();
+			if (nmsplayer.playerConnection != null) {
+				return ConnectionImpl.getFromChannel(nmsplayer.playerConnection.networkManager.channel);
+			}
+		}
 		return ProtocolStorage.getConnection(player.getAddress());
 	}
 
 	public static Connection getConnection(SocketAddress address) {
-		return ProtocolStorage.getConnection(address);
+		Connection connection = ProtocolStorage.getConnection(address);
+		if (connection != null) {
+			return connection;
+		}
+		if (getconnectioncheckall) {
+			for (Connection econnection : getConnections()) {
+				if (econnection.getAddress().equals(address)) {
+					return econnection;
+				}
+			}
+		}
+		return null;
 	}
 
 	public static ItemRemapperControl getItemRemapper(ProtocolVersion version) {
