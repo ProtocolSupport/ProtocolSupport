bf7f5b6b198c0c4305a768c590787f0b9fad7975
Shevchik <Shev4ik.den@gmail.com>
2016-11-23 14:07:39 +0300

    Fix ALL versions helper. Fixes base entity metadata remap for 1.11

--- src/protocolsupport/utils/ProtocolVersionsHelper.java
+++ src/protocolsupport/utils/ProtocolVersionsHelper.java
@@ -34,7 +34,7 @@ public class ProtocolVersionsHelper {
 
 	public static final ProtocolVersion[] BEFORE_1_11 = concat(BEFORE_1_10, ProtocolVersion.MINECRAFT_1_10);
 
-	public static final ProtocolVersion[] ALL = concat(BEFORE_1_10, ProtocolVersion.MINECRAFT_1_10);
+	public static final ProtocolVersion[] ALL = concat(BEFORE_1_11, ProtocolVersion.MINECRAFT_1_11);
 
 	public static final ProtocolVersion[] ALL_1_9 = ProtocolVersion.getAllBetween(ProtocolVersion.MINECRAFT_1_9_4, ProtocolVersion.MINECRAFT_1_9);
 
