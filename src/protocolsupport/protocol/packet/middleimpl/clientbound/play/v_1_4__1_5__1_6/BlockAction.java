15614c3a61961234ed0acd2f027f94c516f0eace
Shevchik <Shev4ik.den@gmail.com>
2016-11-28 00:10:59 +0300

    Skip block action for remapped block for < 1.7

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_1_4__1_5__1_6/BlockAction.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_1_4__1_5__1_6/BlockAction.java
@@ -4,13 +4,19 @@ import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.packet.ClientBoundPacket;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleBlockAction;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
+import protocolsupport.protocol.typeremapper.id.IdRemapper;
 import protocolsupport.utils.recyclable.RecyclableCollection;
+import protocolsupport.utils.recyclable.RecyclableEmptyList;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
 public class BlockAction extends MiddleBlockAction<RecyclableCollection<ClientBoundPacketData>> {
 
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData(ProtocolVersion version) {
+		int blockstate = type << 4;
+		if (IdRemapper.BLOCK.getTable(version).getRemap(blockstate) != blockstate) {
+			return RecyclableEmptyList.get();
+		}
 		ClientBoundPacketData serializer = ClientBoundPacketData.create(ClientBoundPacket.PLAY_BLOCK_ACTION_ID, version);
 		serializer.writeLegacyPositionS(position);
 		serializer.writeByte(info1);
