6fb10094612618bdc9db1f9e36deac771c569b14
Shevchik <Shev4ik.den@gmail.com>
2017-01-19 13:32:24 +0300

    Remove some workarounds. There is a separate event is ps that allows changing remote address if needed.

--- src/protocolsupport/api/ProtocolSupportAPI.java
+++ src/protocolsupport/api/ProtocolSupportAPI.java
@@ -4,20 +4,13 @@ import java.net.SocketAddress;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
-import net.minecraft.server.v1_11_R1.EntityPlayer;
 import protocolsupport.api.remapper.BlockRemapperControl;
 import protocolsupport.api.remapper.ItemRemapperControl;
-import protocolsupport.protocol.ConnectionImpl;
 import protocolsupport.protocol.storage.ProtocolStorage;
-import protocolsupport.utils.Utils;
-import protocolsupport.utils.Utils.Converter;
 
 public class ProtocolSupportAPI {
-
-	private static final boolean getconnectioncheckall = Utils.getJavaPropertyValue("api.getconnection.checkall", false, Converter.STRING_TO_BOOLEAN);
 
 	public static ProtocolVersion getProtocolVersion(Player player) {
 		Connection connection = getConnection(player);
@@ -34,28 +27,11 @@ public class ProtocolSupportAPI {
 	}
 
 	public static Connection getConnection(Player player) {
-		if (player instanceof CraftPlayer) {
-			EntityPlayer nmsplayer = ((CraftPlayer) player).getHandle();
-			if (nmsplayer.playerConnection != null) {
-				return ConnectionImpl.getFromChannel(nmsplayer.playerConnection.networkManager.channel);
-			}
-		}
 		return getConnection(player.getAddress());
 	}
 
 	public static Connection getConnection(SocketAddress address) {
-		Connection connection = ProtocolStorage.getConnection(address);
-		if (connection != null) {
-			return connection;
-		}
-		if (getconnectioncheckall) {
-			for (Connection econnection : getConnections()) {
-				if (econnection.getAddress().equals(address)) {
-					return econnection;
-				}
-			}
-		}
-		return null;
+		return ProtocolStorage.getConnection(address);
 	}
 
 	public static ItemRemapperControl getItemRemapper(ProtocolVersion version) {
