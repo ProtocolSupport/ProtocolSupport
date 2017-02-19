0b2b0e0dd2dbf4483faa5b9a568f8bf0b04195d5
Shevchik <Shev4ik.den@gmail.com>
2017-02-20 02:09:44 +0300

    Do not use google guava

--- src/protocolsupport/protocol/storage/ThrottleTracker.java
+++ src/protocolsupport/protocol/storage/ThrottleTracker.java
@@ -1,32 +1,35 @@
 package protocolsupport.protocol.storage;
 
 import java.net.InetAddress;
-import java.util.concurrent.TimeUnit;
 
 import org.bukkit.Bukkit;
 
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-
-import protocolsupport.utils.Utils;
+import gnu.trove.iterator.TObjectLongIterator;
+import gnu.trove.map.hash.TObjectLongHashMap;
 
 public class ThrottleTracker {
 
 	private static final long time = Bukkit.getConnectionThrottle();
 
-	private static final Cache<InetAddress, Boolean> tracker = CacheBuilder.newBuilder()
-	.concurrencyLevel(Utils.getRawJavaPropertyValue("io.netty.eventLoopThreads", Runtime.getRuntime().availableProcessors(), Utils.Converter.STRING_TO_INT))
-	.expireAfterWrite(time > 0 ? time : 1, TimeUnit.MILLISECONDS)
-	.build();
+	private static final TObjectLongHashMap<InetAddress> tracker = new TObjectLongHashMap<>();
 
 	public static boolean isEnabled() {
 		return time > 0;
 	}
 
 	public static boolean throttle(InetAddress address) {
-		boolean result = tracker.getIfPresent(address) != null;
-		tracker.put(address, Boolean.TRUE);
-		return result;
+		synchronized (tracker) {
+			long ctime = System.currentTimeMillis();
+			TObjectLongIterator<InetAddress> iterator = tracker.iterator();
+			while (iterator.hasNext()) {
+				iterator.advance();
+				if (iterator.value() < ctime) {
+					iterator.remove();
+				}
+			}
+			long ret = tracker.put(address, ctime + time);
+			return ret != tracker.getNoEntryValue();
+		}
 	}
 
 }
