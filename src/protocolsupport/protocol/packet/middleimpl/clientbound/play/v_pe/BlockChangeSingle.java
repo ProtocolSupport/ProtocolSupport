d35cfadb8cbc382b61b4bfd3f6079e2ed98f10c8
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-03-05 21:48:16 -0300

    Fix whitespaces, add PRIORITY flag

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/BlockChangeSingle.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/BlockChangeSingle.java
@@ -12,14 +12,14 @@ public class BlockChangeSingle extends MiddleBlockChangeSingle {
 
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData(ProtocolVersion version) {
-	    ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.UPDATE_BLOCK, version);
+		ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.UPDATE_BLOCK, version);
 		VarNumberSerializer.writeSVarInt(serializer, position.getX());
 		VarNumberSerializer.writeVarInt(serializer, position.getY());
 		VarNumberSerializer.writeSVarInt(serializer, position.getZ());
 		int type = id >> 4;
 		int meta = id & 15;
 		VarNumberSerializer.writeVarInt(serializer, type);
-		VarNumberSerializer.writeVarInt(serializer, meta); // TODO: Flags (are they even used anymore?)
+		VarNumberSerializer.writeVarInt(serializer, (0b1000 << 4) | meta); // 0b1000 = Priority // TODO: Flags (are they even used anymore?)
 		return RecyclableSingletonList.create(serializer);
 	}
 
