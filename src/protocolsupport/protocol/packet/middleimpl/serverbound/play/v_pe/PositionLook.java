42f51288321ab9f281ad4c451cae6f9ab9cde00f
Shevchik <Shev4ik.den@gmail.com>
2017-03-12 06:19:39 +0300

    Also add are flying packet to serverbound position look, so server will accept it and start loading chunks

--- src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_pe/PositionLook.java
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_pe/PositionLook.java
@@ -2,11 +2,30 @@ package protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe;
 
 import io.netty.buffer.ByteBuf;
 import protocolsupport.api.ProtocolVersion;
+import protocolsupport.protocol.packet.ServerBoundPacket;
 import protocolsupport.protocol.packet.middle.serverbound.play.MiddlePositionLook;
+import protocolsupport.protocol.packet.middleimpl.ServerBoundPacketData;
 import protocolsupport.protocol.serializer.MiscSerializer;
 import protocolsupport.protocol.serializer.VarNumberSerializer;
+import protocolsupport.utils.recyclable.RecyclableArrayList;
+import protocolsupport.utils.recyclable.RecyclableCollection;
 
 public class PositionLook extends MiddlePositionLook {
+
+	@Override
+	public RecyclableCollection<ServerBoundPacketData> toNative() {
+		RecyclableArrayList<ServerBoundPacketData> packets = RecyclableArrayList.create();
+		ServerBoundPacketData flying = ServerBoundPacketData.create(ServerBoundPacket.PLAY_PLAYER);
+		flying.writeBoolean(onGround);
+		packets.add(flying);
+		RecyclableCollection<ServerBoundPacketData> superPackets = super.toNative();
+		try {
+			packets.addAll(superPackets);
+		} finally {
+			superPackets.recycleObjectOnly();
+		}
+		return packets;
+	}
 
 	@Override
 	public void readFromClientData(ByteBuf clientdata, ProtocolVersion version) {
