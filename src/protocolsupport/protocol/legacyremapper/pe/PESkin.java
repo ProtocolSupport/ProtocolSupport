a303406df7b8fcfd369f7dbe6d5988dcff56f4b0
7kasper <kasper77077@gmail.com>
2017-03-24 09:49:09 +0100

    Revert Skin Gathering.
    As was pointed out, skin gathering should not be done on the networking
    thread. Maybe something for SkinsRestorer? We'll see.

--- src/protocolsupport/protocol/legacyremapper/pe/PESkin.java
+++ src/protocolsupport/protocol/legacyremapper/pe/PESkin.java
@@ -4,8 +4,6 @@ import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.concurrent.Callable;
 
 import javax.imageio.ImageIO;
@@ -15,28 +13,6 @@ import org.apache.commons.lang3.Validate;
 import protocolsupport.utils.Utils;
 
 public class PESkin {
-	
-	//TODO Fix this :)
-	public static final byte[] getSkin (String username){
-		URL imageLocation;
-		try {
-			imageLocation = new URL("http://s3.amazonaws.com/MinecraftSkins/" + username + ".png");
-		} catch (MalformedURLException e1) {
-			return STEVE;
-		}
-		return new Callable<byte[]>() {
-			@Override
-			public byte[] call() {
-				try {
-					return toNetworkData(ImageIO.read(imageLocation));
-				} catch (Exception e) {
-					return STEVE;
-				}
-			}
-		}.call();
-	}
-	
-	
 	public static void init() {
 	}
 
@@ -53,7 +29,7 @@ public class PESkin {
 
 	public static byte[] toNetworkData(BufferedImage skin) {
 		Validate.isTrue(skin.getWidth() == 64, "Must be 64 pixels wide");
-		Validate.isTrue(skin.getHeight() == 64, "Must be 64 pixels high");
+		Validate.isTrue(skin.getHeight() == 64 || skin.getHeight() == 32 , "Must be 32 or 64 pixels high");
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 		for (int y = 0; y < skin.getHeight(); y++) {
 			for (int x = 0; x < skin.getWidth(); x++) {
