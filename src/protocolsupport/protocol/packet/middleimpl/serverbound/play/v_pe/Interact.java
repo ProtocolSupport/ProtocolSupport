2522b173fa9e6a1502292a55fb4018b55632af78
7kasper <kasper77077@gmail.com>
2017-04-02 15:34:31 +0200

    Updated comments

--- src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_pe/Interact.java
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_pe/Interact.java
@@ -21,13 +21,13 @@ public class Interact extends ServerBoundMiddlePacket{
 	@Override
 	public void readFromClientData(ByteBuf clientdata, ProtocolVersion version) {
 		peAction = clientdata.readUnsignedByte();
-		targetId = (int) VarNumberSerializer.readVarLong(clientdata)/2; //We should look into why pocket doubles the entity ids. For now I just divide it back.
+		targetId = ((int) VarNumberSerializer.readVarLong(clientdata) /2); //We should look into why pocket doubles the entity ids. For now I just divide it back.
 	}
 	
 	private static final int INTERACT = 1; //I think MCW10 does this on every right click. Pocket can only right click when button appears.
 	private static final int ATTACK = 2;
 	private static final int LEAVE_VEHICLE = 3;
-//	private static final int HOVER = 4; //TODO: send the interact button back to PE when looking at entity such as a villager.
+//	private static final int HOVER = 4; //TODO: send the interact button back to PE when looking at entity. Perhaps with update trade packet?
 
 	@Override
 	public RecyclableCollection<ServerBoundPacketData> toNative() {
@@ -35,7 +35,6 @@ public class Interact extends ServerBoundMiddlePacket{
 		WatchedEntity target = cache.getWatchedEntity(targetId);
 		switch(peAction){
 			case INTERACT: {
-				//Some plugins listen to the new interactAt event while others listen to the old event which isn't send when clicking armor stands o_O Best to send both when applicable.
 				if(target.getType() != SpecificRemapper.ARMOR_STAND){
 					packets.add(MiddleUseEntity.create(targetId, MiddleUseEntity.Action.INTERACT, null, 0));
 				}
@@ -45,7 +44,7 @@ public class Interact extends ServerBoundMiddlePacket{
 				packets.add(MiddleUseEntity.create(targetId, MiddleUseEntity.Action.ATTACK, null, 0));
 			}
 			case LEAVE_VEHICLE: {
-				//packets.add(MiddleEntityAction.create(cache.getSelfPlayerEntityId(), 0, 0)); Exit vehicle by sneaking.
+				//packets.add(MiddleEntityAction.create(cache.getSelfPlayerEntityId(), 0, 0)); //Exit vehicle by sneaking.
 			}
 		}
 		return packets;
