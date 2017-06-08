031a5be7625e9db6c7cfd6cb84b591352d478b42
Shevchik <Shev4ik.den@gmail.com>
2017-06-08 14:49:30 +0300

    Update to 1.12

--- src/protocolsupport/protocol/packet/middle/serverbound/play/MiddleCraftingBookData.java
+++ src/protocolsupport/protocol/packet/middle/serverbound/play/MiddleCraftingBookData.java
@@ -1,20 +1,15 @@
 package protocolsupport.protocol.packet.middle.serverbound.play;
 
-import java.text.MessageFormat;
-
 import protocolsupport.protocol.packet.ServerBoundPacket;
 import protocolsupport.protocol.packet.middle.ServerBoundMiddlePacket;
 import protocolsupport.protocol.packet.middleimpl.ServerBoundPacketData;
-import protocolsupport.protocol.serializer.VarNumberSerializer;
+import protocolsupport.protocol.serializer.MiscSerializer;
 import protocolsupport.utils.recyclable.RecyclableCollection;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
 public abstract class MiddleCraftingBookData extends ServerBoundMiddlePacket {
 
-	protected static final int TYPE_DISPLAYED_RECIPE = 1;
-	protected static final int TYPE_CRAFTING_BOOK_STATUS = 2;
-
-	protected int type;
+	protected Type type;
 	protected int recipeId;
 	protected boolean craftingBookOpen;
 	protected boolean recipeFilterEnabled;
@@ -22,22 +17,23 @@ public abstract class MiddleCraftingBookData extends ServerBoundMiddlePacket {
 	@Override
 	public RecyclableCollection<ServerBoundPacketData> toNative() {
 		ServerBoundPacketData creator = ServerBoundPacketData.create(ServerBoundPacket.PLAY_CRAFTING_BOOK_DATA);
-		VarNumberSerializer.writeVarInt(creator, type);
+		MiscSerializer.writeEnum(creator, type);
 		switch (type) {
-			case TYPE_DISPLAYED_RECIPE: {
+			case DISPLAYED_RECIPE: {
 				creator.writeInt(recipeId);
 				break;
 			}
-			case TYPE_CRAFTING_BOOK_STATUS: {
+			case CRAFTING_BOOK_STATUS: {
 				creator.writeBoolean(craftingBookOpen);
 				creator.writeBoolean(recipeFilterEnabled);
 				break;
 			}
-			default: {
-				throw new IllegalArgumentException(MessageFormat.format("Unknown crafting book data type {0}", type));
-			}
 		}
 		return RecyclableSingletonList.create(creator);
+	}
+
+	protected static enum Type {
+		DISPLAYED_RECIPE, CRAFTING_BOOK_STATUS;
 	}
 
 }
--- src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_12/CraftingBookData.java
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_12/CraftingBookData.java
@@ -3,19 +3,19 @@ package protocolsupport.protocol.packet.middleimpl.serverbound.play.v_12;
 import io.netty.buffer.ByteBuf;
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.packet.middle.serverbound.play.MiddleCraftingBookData;
-import protocolsupport.protocol.serializer.VarNumberSerializer;
+import protocolsupport.protocol.serializer.MiscSerializer;
 
 public class CraftingBookData extends MiddleCraftingBookData {
 
 	@Override
 	public void readFromClientData(ByteBuf clientdata, ProtocolVersion version) {
-		type = VarNumberSerializer.readVarInt(clientdata);
+		type = MiscSerializer.readEnum(clientdata, Type.class);
 		switch (type) {
-			case TYPE_DISPLAYED_RECIPE: {
+			case DISPLAYED_RECIPE: {
 				recipeId = clientdata.readInt();
 				break;
 			}
-			case TYPE_CRAFTING_BOOK_STATUS: {
+			case CRAFTING_BOOK_STATUS: {
 				craftingBookOpen = clientdata.readBoolean();
 				recipeFilterEnabled = clientdata.readBoolean();
 				break;
