6455946cb68e161f5ca13fa19a08a98edbd15174
Shevchik <Shev4ik.den@gmail.com>
2017-06-15 23:03:42 +0300

    Refactor set title

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/Title.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/Title.java
@@ -1,64 +1,60 @@
 package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_pe;
 
-import net.md_5.bungee.api.chat.BaseComponent;
-import net.md_5.bungee.chat.ComponentSerializer;
+import io.netty.handler.codec.EncoderException;
 import protocolsupport.api.ProtocolVersion;
+import protocolsupport.api.chat.ChatAPI;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleTitle;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
 import protocolsupport.protocol.serializer.StringSerializer;
 import protocolsupport.protocol.serializer.VarNumberSerializer;
 import protocolsupport.protocol.typeremapper.pe.PEPacketIDs;
-import protocolsupport.utils.recyclable.RecyclableArrayList;
 import protocolsupport.utils.recyclable.RecyclableCollection;
+import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
 public class Title extends MiddleTitle {
+
 	private static final int HIDE = 0;
 	private static final int RESET = 1;
 	private static final int SET_TITLE = 2;
 	private static final int SET_SUBTITLE = 3;
 	private static final int SET_ACTION_BAR = 4;
 	private static final int SET_TIMINGS = 5;
-	
+
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData(ProtocolVersion version) {
-		RecyclableArrayList<ClientBoundPacketData> packets = RecyclableArrayList.create();
-		ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.SET_TITLE, version);
-		int action = 0;
 		switch (this.action) {
-		case HIDE:
-			action = Title.HIDE;
-			break;
-		case RESET:
-			action = Title.RESET;
-			break;
-		case SET_ACTION_BAR:
-			action = Title.SET_ACTION_BAR;
-			break;
-		case SET_SUBTITLE:
-			action = Title.SET_SUBTITLE;
-			break;
-		case SET_TIMES:
-			action = Title.SET_TIMINGS;
-			break;
-		case SET_TITLE:
-			action = Title.SET_TITLE;
-			break;
-		}
-		VarNumberSerializer.writeSVarInt(serializer, action);
-		String toBeSent = action == SET_SUBTITLE ? this.subtitleJson : this.titleJson;
-		if (toBeSent == null) { toBeSent = ""; }; // toBeSent should never be null since we need to parse it later
-		String legacyText = "";
-		BaseComponent[] textComponent = ComponentSerializer.parse(toBeSent);
-		for (BaseComponent bc : textComponent) {
-			if (bc != null) {
-				legacyText += bc.toLegacyText();
+			case HIDE: {
+				return RecyclableSingletonList.create(create(version, HIDE, "", 0, 0, 0));
+			}
+			case RESET: {
+				return RecyclableSingletonList.create(create(version, RESET, "", 0, 0, 0));
+			}
+			case SET_ACTION_BAR: {
+				return RecyclableSingletonList.create(create(version, SET_ACTION_BAR, ChatAPI.fromJSON(titleJson).toLegacyText(), 0, 0, 0));
+			}
+			case SET_SUBTITLE: {
+				return RecyclableSingletonList.create(create(version, SET_SUBTITLE, ChatAPI.fromJSON(subtitleJson).toLegacyText(), 0, 0, 0));
+			}
+			case SET_TIMES: {
+				return RecyclableSingletonList.create(create(version, SET_TIMINGS, "", fadeIn, stay, fadeOut));
+			}
+			case SET_TITLE: {
+				return RecyclableSingletonList.create(create(version, SET_TITLE, ChatAPI.fromJSON(titleJson).toLegacyText(), 0, 0, 0));
+			}
+			default: {
+				throw new EncoderException("Should not reach here");
 			}
 		}
-		StringSerializer.writeString(serializer, version, legacyText);
-		VarNumberSerializer.writeSVarInt(serializer, this.fadeIn);
-		VarNumberSerializer.writeSVarInt(serializer, this.stay);
-		VarNumberSerializer.writeSVarInt(serializer, this.fadeOut);
-		packets.add(serializer);
-		return packets;
 	}
+
+	private static ClientBoundPacketData create(ProtocolVersion version, int action, String text, int fadeIn, int stay, int fadeOut) {
+		ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.SET_TITLE, version);
+		VarNumberSerializer.writeSVarInt(serializer, action);
+		StringSerializer.writeString(serializer, version, text);
+		VarNumberSerializer.writeSVarInt(serializer, fadeIn);
+		VarNumberSerializer.writeSVarInt(serializer, stay);
+		VarNumberSerializer.writeSVarInt(serializer, fadeOut);
+		return serializer;
+	}
+
 }
