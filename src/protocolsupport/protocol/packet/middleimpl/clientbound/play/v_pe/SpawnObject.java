8f40345d3b0447c448c5d1fbbcc20902358470fa
7kasper <kasper77077@gmail.com>
2017-07-19 21:41:19 +0200

    Added prepareItem implementation.

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/SpawnObject.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/SpawnObject.java
@@ -1,7 +1,5 @@
 package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_pe;
 
-import org.bukkit.Material;
-
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleSpawnObject;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
@@ -13,44 +11,74 @@ import protocolsupport.protocol.typeremapper.pe.PEDataValues;
 import protocolsupport.protocol.typeremapper.pe.PEPacketIDs;
 import protocolsupport.protocol.utils.types.NetworkEntityType;
 import protocolsupport.utils.recyclable.RecyclableCollection;
+import protocolsupport.utils.recyclable.RecyclableEmptyList;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
-import protocolsupport.zplatform.ServerPlatform;
 import protocolsupport.zplatform.itemstack.ItemStackWrapper;
 
 public class SpawnObject extends MiddleSpawnObject {
-
+	
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData(ProtocolVersion version) {
 		if(entity.getType() == NetworkEntityType.ITEM) {
-			return RecyclableSingletonList.create(createItem(version, entity.getId(), x, y, z, motX / 8.000F, motY / 8000.F, motZ / 8000.F));
+			//We need to prepare the item because we can only spawn it after we've received the first metadata update.
+			cache.prepareItem(new PreparedItem(entity.getId(), x, y, z, motX / 8.000F, motY / 8000.F, motZ / 8000.F));
+			return RecyclableEmptyList.get();
 		} else { 
 			return RecyclableSingletonList.create(SpawnLiving.create(
 				version,
-				entity.getId(), x, y, z,
-				motX / 8.000F, motY / 8000.F, motZ / 8000.F, pitch, yaw,
-				null, PEDataValues.getObjectEntityTypeId(IdRemapper.ENTITY.getTable(version).getRemap(entity.getType()))
+				entity.getId(), 
+				x, y, z,
+				motX / 8.000F, motY / 8000.F, motZ / 8000.F, 
+				pitch, yaw,
+				null, 
+				PEDataValues.getObjectEntityTypeId(IdRemapper.ENTITY.getTable(version).getRemap(entity.getType()))
 			));
 		}
-		//TODO: Implement Objectdata
 	}
 	
-	public static ClientBoundPacketData createItem(ProtocolVersion version, int entityId, double x, double y, double z, float motX, float motY, float motZ) {
-		ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.ADD_ITEM_ENTITY, version);
-		VarNumberSerializer.writeSVarLong(serializer, entityId);
-		VarNumberSerializer.writeVarLong(serializer, entityId);
-		//Prepare a fake itemStack to send. The next metadata update will fix it. (We don't know anything about the item yet)
-		ItemStackWrapper fakestack = ServerPlatform.get().getWrapperFactory().createItemStack(Material.STONE);
-		ItemStackSerializer.writePeSlot(serializer, version, fakestack);
-		MiscSerializer.writeLFloat(serializer, (float) x);
-		MiscSerializer.writeLFloat(serializer, (float) y);
-		MiscSerializer.writeLFloat(serializer, (float) z);
-		MiscSerializer.writeLFloat(serializer, motX); 
-		MiscSerializer.writeLFloat(serializer, motY);
-		MiscSerializer.writeLFloat(serializer, motZ);
-		VarNumberSerializer.writeVarInt(serializer, 0);
-		return serializer;
+	public class PreparedItem {
+		
+		private int entityId;
+		private double x; 
+		private double y;
+		private double z;
+		private float motX;
+		private float motY;
+		private float motZ;
+		private ItemStackWrapper itemstack;
+		
+		PreparedItem(int entityId, double x, double y, double z, float motX, float motY, float motZ) {
+			this.entityId = entityId;
+			this.x = x;
+			this.y = y;
+			this.z = z;
+			this.motX = motX;
+			this.motY = motY;
+			this.motZ = motZ;
+		}
+		
+		public int getId() {
+			return entityId;
+		}
+		
+		public void setItemStack(ItemStackWrapper itemstack) {
+			this.itemstack = itemstack;
+		}
+		
+		public ClientBoundPacketData getSpawnPacket(ProtocolVersion version) {
+			ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.ADD_ITEM_ENTITY, version);
+			VarNumberSerializer.writeSVarLong(serializer, entityId);
+			VarNumberSerializer.writeVarLong(serializer, entityId);
+			ItemStackSerializer.writePeSlot(serializer, version, itemstack);
+			MiscSerializer.writeLFloat(serializer, (float) x);
+			MiscSerializer.writeLFloat(serializer, (float) y);
+			MiscSerializer.writeLFloat(serializer, (float) z);
+			MiscSerializer.writeLFloat(serializer, motX); 
+			MiscSerializer.writeLFloat(serializer, motY);
+			MiscSerializer.writeLFloat(serializer, motZ);
+			VarNumberSerializer.writeVarInt(serializer, 0);
+			return serializer;
+		}
 	}
-	
-	
 
 }
