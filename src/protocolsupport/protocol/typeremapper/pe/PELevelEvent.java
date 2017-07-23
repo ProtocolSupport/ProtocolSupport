14392f6bef4922deb188ed3563d5a1085216a5d9
7kasper <kasper77077@gmail.com>
2017-07-23 21:48:58 +0200

    Readded unimplemented sounds, becasue some WHERE implmented :F

--- src/protocolsupport/protocol/typeremapper/pe/PELevelEvent.java
+++ src/protocolsupport/protocol/typeremapper/pe/PELevelEvent.java
@@ -7,6 +7,40 @@ import protocolsupport.protocol.serializer.VarNumberSerializer;
 
 public class PELevelEvent {
 	//TODO: recheck all ids for wrong and missing ones
+	public static final int SOUND_CLICK = 1000;
+	public static final int SOUND_CLICK_FAIL = 1001;
+	public static final int SOUND_SHOOT = 1002;
+	public static final int SOUND_DOOR = 1003;
+	public static final int SOUND_FIZZ = 1004;
+	public static final int SOUND_TNT = 1005;
+	public static final int SOUND_GHAST = 1007;
+	public static final int SOUND_BLAZE_SHOOT = 1008;
+	public static final int SOUND_GHAST_SHOOT = 1009;
+	public static final int SOUND_DOOR_BUMP = 1010;
+	public static final int SOUND_DOOR_CRASH = 1012;
+	public static final int SOUND_ENDERMAN_TELEPORT = 1018;
+	public static final int SOUND_ANVIL_BREAK = 1020;
+	public static final int SOUND_ANVIL_USE = 1021;
+	public static final int SOUND_ANVIL_FALL = 1022;
+	public static final int SOUND_ITEM_DROP = 1030;
+	public static final int SOUND_ITEM_THROWN = 1031;
+	public static final int SOUND_PORTAL = 1032;
+	public static final int SOUND_ITEM_FRAME_ITEM_ADDED = 1040;
+	public static final int SOUND_ITEM_FRAME_PLACED = 1041;
+	public static final int SOUND_ITEM_FRAME_REMOVED = 1042;
+	public static final int SOUND_ITEM_FRAME_ITEM_REMOVED = 1043;
+	public static final int SOUND_ITEM_FRAME_ITEM_ROTATED = 1044;
+	public static final int SOUND_CAMERA_TAKE_PICTURE = 1050;
+	public static final int SOUND_EXPERIENCE_ORB = 1051;
+	public static final int SOUND_BLOCK_PLACE = 1052;
+	public static final int SOUND_BUTTON_CLICK = 3500;
+	public static final int SOUND_EXPLODE = 3501;
+	public static final int SOUND_SPLASH = 3506;
+	public static final int SOUND_CAULDRON = 3501;
+	public static final int SOUND_CAULDRON_DYE_ARMOR = 3502;
+	public static final int SOUND_CAULDRON_FILL_POTION = 3504;
+	public static final int SOUND_CAULDRON_FILL_WATER = 3506;
+	
 	public static final int GUARDIAN_CURSE = 2006;
 	
 	public static final int CAULDRON_DYE_ARMOR = 3502;
