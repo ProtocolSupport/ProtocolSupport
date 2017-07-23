80f307b09d7f25256fca50ffc341fa61790815f7
7kasper <kasper77077@gmail.com>
2017-07-23 15:13:41 +0200

    Added explosion packet

---
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/Explosion.java
@@ -0,0 +1,38 @@
+package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_pe;
+
+import protocolsupport.api.ProtocolVersion;
+import protocolsupport.protocol.packet.middle.clientbound.play.MiddleExplosion;
+import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
+import protocolsupport.protocol.serializer.MiscSerializer;
+import protocolsupport.protocol.serializer.PositionSerializer;
+import protocolsupport.protocol.serializer.VarNumberSerializer;
+import protocolsupport.protocol.typeremapper.pe.PEPacketIDs;
+import protocolsupport.protocol.utils.types.Position;
+import protocolsupport.utils.recyclable.RecyclableArrayList;
+import protocolsupport.utils.recyclable.RecyclableCollection;
+
+public class Explosion extends MiddleExplosion {
+
+	@Override
+	public RecyclableCollection<ClientBoundPacketData> toData(ProtocolVersion version) {
+		RecyclableArrayList<ClientBoundPacketData> packets = RecyclableArrayList.create();
+		packets.add(create(version, x, y , z, radius, blocks));
+		packets.add(EntityVelocity.create(version, cache.getSelfPlayerEntityId(), pMotX, pMotY, pMotZ));
+		return packets;
+	}
+	
+	public static ClientBoundPacketData create(ProtocolVersion version, float x, float y, float z, float radius, Position[] blocks) {
+		ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.EXPLODE, version);
+		MiscSerializer.writeLFloat(serializer, x);
+		MiscSerializer.writeLFloat(serializer, y);
+		MiscSerializer.writeLFloat(serializer, z);
+		MiscSerializer.writeLFloat(serializer, radius);
+		VarNumberSerializer.writeVarInt(serializer, blocks.length);
+		for (Position b : blocks) {
+			PositionSerializer.writePEPosition(serializer, b);
+		}
+		return serializer;
+	}
+
+
+}
