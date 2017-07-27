e667fa5992b85647e8525ba76deb19b2f2e42d9b
Shevchik <Shev4ik.den@gmail.com>
2017-07-28 00:54:29 +0300

    How did this patch dissappear?

--- src/protocolsupport/protocol/storage/ProtocolStorage.java
+++ src/protocolsupport/protocol/storage/ProtocolStorage.java
@@ -29,7 +29,7 @@ public class ProtocolStorage {
 	}
 
 	public static ConnectionImpl getConnection(SocketAddress address) {
-		return secondaryStorage.get(address);
+		return address != null ? secondaryStorage.get(address) : null;
 	}
 
 	public static ConnectionImpl removeConnection(SocketAddress address) {
