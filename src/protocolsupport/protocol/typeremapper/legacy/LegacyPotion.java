ac369a821aef5a67c9fc8e0185e95476c97755c1
Shevchik <Shev4ik.den@gmail.com>
2017-07-29 03:46:10 +0300

    Use correct legacy potion ids

--- src/protocolsupport/protocol/typeremapper/legacy/LegacyPotion.java
+++ src/protocolsupport/protocol/typeremapper/legacy/LegacyPotion.java
@@ -9,25 +9,25 @@ public class LegacyPotion {
 	private static final TObjectIntHashMap<String> toLegacyId = new TObjectIntHashMap<>();
 	private static final TIntObjectHashMap<String> fromLegacyId = new TIntObjectHashMap<>();
 	static {
-		register("night_vision", 8198);
+		register("night_vision", 8230);
 		register("long_night_vision", 8262);
-        register("invisibility", 8206);
+        register("invisibility", 8238);
         register("long_invisibility", 8270);
         register("leaping", 8203);
         register("long_leaping", 8267);
         register("strong_leaping", 8235);
-        register("fire_resistance", 8195);
+        register("fire_resistance", 8227);
         register("long_fire_resistance", 8259);
         register("swiftness", 8194);
         register("long_swiftness", 8258);
         register("strong_swiftness", 8226);
-        register("slowness", 8202);
+        register("slowness", 8234);
         register("long_slowness", 8266);
-        register("water_breathing", 8205);
+        register("water_breathing", 8237);
         register("long_water_breathing", 8269);
-        register("healing", 8197);
+        register("healing", 8261);
         register("strong_healing", 8229);
-        register("harming", 8204);
+        register("harming", 8268);
         register("strong_harming", 8236);
         register("poison", 8196);
         register("long_poison", 8260);
@@ -38,7 +38,7 @@ public class LegacyPotion {
         register("strength", 8201);
         register("long_strength", 8265);
         register("strong_strength", 8233);
-        register("weakness", 8200);
+        register("weakness", 8232);
         register("long_weakness", 8264);
 	}
 
