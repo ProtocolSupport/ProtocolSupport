5fd4f458a48d5591e62773229ef8491c0ecda635
7kasper <kasper77077@gmail.com>
2017-08-01 12:08:00 +0200

    Updated packet Ids.

--- src/protocolsupport/protocol/pipeline/version/v_pe/PEPacketDecoder.java
+++ src/protocolsupport/protocol/pipeline/version/v_pe/PEPacketDecoder.java
@@ -12,11 +12,11 @@ import protocolsupport.protocol.packet.middleimpl.ServerBoundPacketData;
 import protocolsupport.protocol.packet.middleimpl.serverbound.handshake.v_pe.ClientLogin;
 import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.Animation;
 import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.Chat;
-import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.InstantBlockBreak;
+//import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.InstantBlockBreak;
 import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.Interact;
 import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.PlayerAction;
 import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.PositionLook;
-import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.UseItem;
+//import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.UseItem;
 import protocolsupport.protocol.pipeline.version.AbstractPacketDecoder;
 import protocolsupport.protocol.storage.NetworkDataCache;
 import protocolsupport.protocol.typeremapper.pe.PEPacketIDs;
@@ -37,8 +37,8 @@ public class PEPacketDecoder extends AbstractPacketDecoder {
 		registry.register(NetworkState.PLAY, PEPacketIDs.CHAT, Chat.class);
 		registry.register(NetworkState.PLAY, PEPacketIDs.ANIMATION, Animation.class);
 		registry.register(NetworkState.PLAY, PEPacketIDs.INTERACT, Interact.class);
-		registry.register(NetworkState.PLAY, PEPacketIDs.REMOVE_BLOCK, InstantBlockBreak.class);
-		registry.register(NetworkState.PLAY, PEPacketIDs.USE_ITEM, UseItem.class);
+		//registry.register(NetworkState.PLAY, PEPacketIDs.REMOVE_BLOCK, InstantBlockBreak.class);
+		//registry.register(NetworkState.PLAY, PEPacketIDs.USE_ITEM, UseItem.class);
 	}
 
 	public PEPacketDecoder(Connection connection, NetworkDataCache cache) {
