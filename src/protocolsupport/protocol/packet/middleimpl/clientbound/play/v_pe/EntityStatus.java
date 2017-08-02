fc583400d07c100522bbe88525ae8f93eb022499
7kasper <kasper77077@gmail.com>
2017-08-02 10:31:56 +0200

    Fixes for yesterday.

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/EntityStatus.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/EntityStatus.java
@@ -1,8 +1,6 @@
 package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_pe;
 
-import java.util.Arrays;
-import java.util.List;
-
+import gnu.trove.set.hash.TIntHashSet;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleEntityStatus;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
 import protocolsupport.protocol.serializer.VarNumberSerializer;
@@ -13,15 +11,14 @@ import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
 public class EntityStatus extends MiddleEntityStatus {
 
-	List<Integer> allowedIds = Arrays.asList(2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 31, 34, 57, 63);
+	//TODO: Actually remap and skip the status codes. It seems that with the new update PE crashes if ID is unknown.
+	TIntHashSet allowedIds = new TIntHashSet(new int[] {2, 3, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 31, 34, 57, 63});
 	
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData() {
 		if(allowedIds.contains(status)) {
 			ClientBoundPacketData serializer = ClientBoundPacketData.create(PEPacketIDs.ENTITY_EVENT, connection.getVersion());
 			VarNumberSerializer.writeVarLong(serializer, entityId);
-			// TODO: Update status codes when added. Remap status codes.
-			// Check, but so far no tested unknown status codes break PE.
 			switch (cache.getWatchedEntity(entityId).getType()) {
 				case FISHING_FLOAT: {
 					if (status == 31) {
