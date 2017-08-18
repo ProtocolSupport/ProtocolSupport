a5cf199f7f7f59bb184dd61c403679c2c7e991e0
Shevchik <Shev4ik.den@gmail.com>
2017-08-18 10:39:02 +0300

    No longer send hotbar and sys messages as chat messages to clients that don't support them

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_4_5/Chat.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_4_5/Chat.java
@@ -1,17 +1,22 @@
 package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_4_5;
 
 import protocolsupport.api.ProtocolVersion;
+import protocolsupport.api.chat.ChatAPI.MessagePosition;
 import protocolsupport.protocol.packet.ClientBoundPacket;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleChat;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
 import protocolsupport.protocol.serializer.StringSerializer;
 import protocolsupport.utils.recyclable.RecyclableCollection;
+import protocolsupport.utils.recyclable.RecyclableEmptyList;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
 public class Chat extends MiddleChat {
 
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData() {
+		if (position != MessagePosition.CHAT) {
+			return RecyclableEmptyList.get();
+		}
 		ProtocolVersion version = connection.getVersion();
 		ClientBoundPacketData serializer = ClientBoundPacketData.create(ClientBoundPacket.PLAY_CHAT_ID, version);
 		StringSerializer.writeString(serializer, version, message.toLegacyText(cache.getLocale()));
--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_6/Chat.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_6/Chat.java
@@ -3,18 +3,23 @@ package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_6;
 import com.google.gson.JsonObject;
 
 import protocolsupport.api.ProtocolVersion;
+import protocolsupport.api.chat.ChatAPI.MessagePosition;
 import protocolsupport.protocol.packet.ClientBoundPacket;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleChat;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
 import protocolsupport.protocol.serializer.StringSerializer;
 import protocolsupport.utils.Utils;
 import protocolsupport.utils.recyclable.RecyclableCollection;
+import protocolsupport.utils.recyclable.RecyclableEmptyList;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
 public class Chat extends MiddleChat {
 
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData() {
+		if (position != MessagePosition.CHAT) {
+			return RecyclableEmptyList.get();
+		}
 		ProtocolVersion version = connection.getVersion();
 		ClientBoundPacketData serializer = ClientBoundPacketData.create(ClientBoundPacket.PLAY_CHAT_ID, version);
 		StringSerializer.writeString(serializer, version, encode(message.toLegacyText(cache.getLocale())));
