2c50cf7589025c068b5bfed2be4edd4723e709e2
Leonardo Malaman <leonardomalaman@gmail.com>
2017-08-19 11:57:58 -0300

    Add sub ID remapping for items, fix some remaps due to ID differences and some small fixes

--- src/protocolsupport/protocol/typeremapper/itemstack/toclient/ItemIdToPEIdSpecificRemapper.java
+++ src/protocolsupport/protocol/typeremapper/itemstack/toclient/ItemIdToPEIdSpecificRemapper.java
@@ -3,13 +3,32 @@ package protocolsupport.protocol.typeremapper.itemstack.toclient;
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.typeremapper.itemstack.ItemStackSpecificRemapper;
 import protocolsupport.protocol.typeremapper.pe.PEDataValues;
+import protocolsupport.utils.IntTuple;
 import protocolsupport.zplatform.itemstack.ItemStackWrapper;
 
 public class ItemIdToPEIdSpecificRemapper implements ItemStackSpecificRemapper {
 
 	@Override
 	public ItemStackWrapper remap(ProtocolVersion version, String locale, ItemStackWrapper itemstack) {
-		itemstack.setTypeId(PEDataValues.ITEM_ID.getRemap(itemstack.getTypeId()));
+		IntTuple itemAndData = PEDataValues.ITEM_ID.getRemap(itemstack.getTypeId(), itemstack.getData());
+
+		if (itemAndData == null) { // If itemAndData is null, then we are going to get the "-1" remapper
+			// When a remapper uses -1, it means it is a "remap every sub ID to this new ID"
+			itemAndData = PEDataValues.ITEM_ID.getRemap(itemstack.getTypeId(), -1);
+		}
+
+		int itemId = itemstack.getTypeId();
+		int itemData = itemstack.getData();
+
+		if (itemAndData != null) { // If it isn't null, then we are going to get the new remap
+			itemId = itemAndData.getI1();
+			// If it is -1, then we are going to keep the original sub ID, since it shouldn't be remapped
+			itemData = itemAndData.getI2() == -1 ? itemstack.getTypeId() : itemAndData.getI2();
+		}
+
+		itemstack.setTypeId(itemId);
+		itemstack.setData(itemData);
+
 		return itemstack;
 	}
 
