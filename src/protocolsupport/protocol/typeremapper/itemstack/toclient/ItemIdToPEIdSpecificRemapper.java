2bd6dee51e32d2c4d64f4dde81907d65465d3bd1
MrPowerGamerBR <leonardomalaman@gmail.com>
2017-08-19 12:29:14 -0300

    rip comments

--- src/protocolsupport/protocol/typeremapper/itemstack/toclient/ItemIdToPEIdSpecificRemapper.java
+++ src/protocolsupport/protocol/typeremapper/itemstack/toclient/ItemIdToPEIdSpecificRemapper.java
@@ -12,17 +12,15 @@ public class ItemIdToPEIdSpecificRemapper implements ItemStackSpecificRemapper {
 	public ItemStackWrapper remap(ProtocolVersion version, String locale, ItemStackWrapper itemstack) {
 		IntTuple itemAndData = PEDataValues.ITEM_ID.getRemap(itemstack.getTypeId(), itemstack.getData());
 
-		if (itemAndData == null) { // If itemAndData is null, then we are going to get the "-1" remapper
-			// When a remapper uses -1, it means it is a "remap every sub ID to this new ID"
+		if (itemAndData == null) {
 			itemAndData = PEDataValues.ITEM_ID.getRemap(itemstack.getTypeId(), -1);
 		}
 
 		int itemId = itemstack.getTypeId();
 		int itemData = itemstack.getData();
 
-		if (itemAndData != null) { // If it isn't null, then we are going to get the new remap
+		if (itemAndData != null) {
 			itemId = itemAndData.getI1();
-			// If it is -1, then we are going to keep the original sub ID, since it shouldn't be remapped
 			itemData = itemAndData.getI2() == -1 ? itemstack.getTypeId() : itemAndData.getI2();
 		}
 
