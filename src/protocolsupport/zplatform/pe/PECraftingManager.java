6a544c319a60dfee99bf956e18e996a12a0ff8cf
DaMatrix <joey.rabil@gmail.com>
2017-08-24 21:32:01 +0200

    can actually join

--- src/protocolsupport/zplatform/pe/PECraftingManager.java
+++ src/protocolsupport/zplatform/pe/PECraftingManager.java
@@ -2,10 +2,8 @@ package protocolsupport.zplatform.pe;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import net.minecraft.server.v1_12_R1.Block;
-import net.minecraft.server.v1_12_R1.Item;
-import net.minecraft.server.v1_12_R1.ItemStack;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
@@ -16,9 +14,12 @@ import protocolsupport.protocol.serializer.MiscSerializer;
 import protocolsupport.protocol.serializer.VarNumberSerializer;
 import protocolsupport.protocol.typeremapper.itemstack.ItemStackRemapper;
 import protocolsupport.utils.IntTuple;
-import protocolsupport.zplatform.impl.spigot.itemstack.SpigotItemStackWrapper;
+import protocolsupport.zplatform.impl.spigot.itemstack.SpigotNBTTagCompoundWrapper;
+import protocolsupport.zplatform.itemstack.ItemStackWrapper;
+import protocolsupport.zplatform.itemstack.NBTTagCompoundWrapper;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 public class PECraftingManager {
@@ -51,14 +52,14 @@ public class PECraftingManager {
                 Map<Character, org.bukkit.inventory.ItemStack> map = shapedRecipe.getIngredientMap(); //caching for SPEEEEEEED
                 String[] pattern = shapedRecipe.getShape(); //caching for SPEEEEEEED
                 int width = pattern[0].length(), height = pattern.length;
-                SpigotItemStackWrapper[] required = new SpigotItemStackWrapper[width * height];
+                ItemStackWrapper[] required = new RecipeItemStackWrapper[width * height];
                 for (int z = 0; z < height; ++z) {
                     for (int x = 0; x < width; ++x) {
                         int i = z * x;
                         char key = pattern[z].charAt(x);
                         try {
                             org.bukkit.inventory.ItemStack stack = map.get(key);
-                            required[i] = stack == null || stack.getTypeId() < 1 ? (SpigotItemStackWrapper) SpigotItemStackWrapper.create(0) : fromBukkitStack(stack);
+                            required[i] = stack == null || stack.getTypeId() < 1 ? RecipeItemStackWrapper.createNull() : fromBukkitStack(stack);
                         } catch (NullPointerException e)    {
                             ProtocolSupport.logInfo("[WARN] Unable to locate key " + key + " for recipe with output " + recipe.getResult().toString());
                             return;
@@ -68,7 +69,7 @@ public class PECraftingManager {
                 addRecipeShaped(fromBukkitStack(shapedRecipe.getResult()), width, height, required);
             } else if (recipe instanceof ShapelessRecipe)   {
                 ShapelessRecipe shapelessRecipe = (ShapelessRecipe) recipe;
-                SpigotItemStackWrapper[] required = new SpigotItemStackWrapper[shapelessRecipe.getIngredientList().size()];
+                ItemStackWrapper[] required = new RecipeItemStackWrapper[shapelessRecipe.getIngredientList().size()];
                 for (int i = 0; i < required.length; i++)   {
                     required[i] = fromBukkitStack(shapelessRecipe.getIngredientList().get(i));
                 }
@@ -90,47 +91,16 @@ public class PECraftingManager {
         ProtocolSupport.logInfo("Done! Processed " + recipeCount + " recipes!");
     }
 
-    public SpigotItemStackWrapper fromBukkitStack(org.bukkit.inventory.ItemStack stack) {
-        //ProtocolSupport.logInfo(stack.toString());
-        Item i = Item.getById(
-                stack.getType().getId());
-        if (i != null)  {
-            return new SpigotItemStackWrapper(new ItemStack(i, stack.getAmount(), stack.getDurability()));
-        }
-        Block b = Block.getById(stack.getTypeId());
-        return new SpigotItemStackWrapper(new ItemStack(b, stack.getAmount(), stack.getDurability()));
+    public ItemStackWrapper fromBukkitStack(org.bukkit.inventory.ItemStack stack) {
+        return new RecipeItemStackWrapper(stack);
     }
 
-    public int getId(Block b)   {
-        return Block.getId(b);
-    }
-
-    public int getId(Item i)   {
-        return Item.getId(i);
-    }
-
-    public int[] getIds(Object... required) {
-        int[] result = new int[required.length];
-        for (int i = 0; i < required.length; i++)   {
-            Object o = required[i];
-            if (o instanceof Block) {
-                result[i] = getId((Block) o);
-            } else if (o instanceof Item) {
-                result[i] = getId((Item) o);
-            } else {
-                result[i] = 0;
-            }
-        }
-
-        return result;
-    }
-
-    public void addRecipeShaped(SpigotItemStackWrapper output, int width, int height, SpigotItemStackWrapper[] required)    {
+    public void addRecipeShaped(ItemStackWrapper output, int width, int height, ItemStackWrapper[] required)    {
         recipeCount++;
         VarNumberSerializer.writeSVarInt(byteBuf, 1); //type
         VarNumberSerializer.writeSVarInt(byteBuf, width);
         VarNumberSerializer.writeSVarInt(byteBuf, height);
-        for (SpigotItemStackWrapper stack : required) {
+        for (ItemStackWrapper stack : required) {
             ItemStackSerializer.writeItemStack(byteBuf, ProtocolVersion.MINECRAFT_PE, "en_US", stack, true);
         }
         VarNumberSerializer.writeVarInt(byteBuf, 1); //not sure but pocketmine has it
@@ -138,11 +108,11 @@ public class PECraftingManager {
         MiscSerializer.writeUUID(byteBuf, UUID.nameUUIDFromBytes(byteBuf.array()));
     }
 
-    public void addRecipeShapeless(SpigotItemStackWrapper output, SpigotItemStackWrapper[] required)    {
+    public void addRecipeShapeless(ItemStackWrapper output, ItemStackWrapper[] required)    {
         recipeCount++;
         VarNumberSerializer.writeSVarInt(byteBuf, 0);
         VarNumberSerializer.writeVarInt(byteBuf, required.length);
-        for (SpigotItemStackWrapper stack : required) {
+        for (ItemStackWrapper stack : required) {
             ItemStackSerializer.writeItemStack(byteBuf, ProtocolVersion.MINECRAFT_PE, "en_US", stack, true);
         }
         VarNumberSerializer.writeVarInt(byteBuf, 1); //not sure but pocketmine has it
@@ -150,7 +120,7 @@ public class PECraftingManager {
         MiscSerializer.writeUUID(byteBuf, UUID.nameUUIDFromBytes(byteBuf.array()));
     }
 
-    public void addRecipeFurnace(SpigotItemStackWrapper output, SpigotItemStackWrapper input)   {
+    public void addRecipeFurnace(ItemStackWrapper output, ItemStackWrapper input)   {
         IntTuple iddata = ItemStackRemapper.ID_DATA_REMAPPING_REGISTRY.getTable(ProtocolVersion.MINECRAFT_PE).getRemap(input.getTypeId(), input.getData());
         if (iddata != null) {
             input.setTypeId(iddata.getI1());
@@ -168,6 +138,113 @@ public class PECraftingManager {
             VarNumberSerializer.writeSVarInt(byteBuf, input.getTypeId());
             VarNumberSerializer.writeSVarInt(byteBuf, input.getData());
             ItemStackSerializer.writeItemStack(byteBuf, ProtocolVersion.MINECRAFT_PE, "en_US", output, true);
+        }
+    }
+
+    private static class RecipeItemStackWrapper extends ItemStackWrapper   {
+        protected final org.bukkit.inventory.ItemStack itemstack;
+        public RecipeItemStackWrapper(org.bukkit.inventory.ItemStack itemstack) {
+            this.itemstack = itemstack;
+        }
+
+        public static RecipeItemStackWrapper createNull() {
+            return create(Material.AIR.getId());
+        }
+
+        public static RecipeItemStackWrapper create(int typeId) {
+            return new RecipeItemStackWrapper(new org.bukkit.inventory.ItemStack(Material.getMaterial(typeId)));
+        }
+
+        @Override
+        public org.bukkit.inventory.ItemStack asBukkitMirror() {
+            return itemstack;
+        }
+
+        @Override
+        public boolean isNull() {
+            return itemstack == null || itemstack.getType() == Material.AIR;
+        }
+
+        @Override
+        public int getTypeId() {
+            return itemstack.getTypeId();
+        }
+
+        @Override
+        @SuppressWarnings("deprecation")
+        public Material getType() {
+            return itemstack.getType();
+        }
+
+        @Override
+        @SuppressWarnings("deprecation")
+        public void setTypeId(int typeId) {
+            Material material = Material.getMaterial(typeId);
+            itemstack.setType(material == null ? Material.AIR : material);
+        }
+
+        @Override
+        @SuppressWarnings("deprecation")
+        public void setType(Material material) {
+            setTypeId(material.getId());
+        }
+
+        @Override
+        public int getData() {
+            return itemstack.getData().getData();
+        }
+
+        @Override
+        public void setData(int data) {
+            itemstack.getData().setData((byte) data);
+        }
+
+        @Override
+        public int getAmount() {
+            return itemstack.getAmount();
+        }
+
+        @Override
+        public void setAmount(int amount) {
+            itemstack.setAmount(amount);
+        }
+
+        @Override
+        public void setDisplayName(String displayName) {
+        }
+
+        @Override
+        public SpigotNBTTagCompoundWrapper getTag() {
+            return SpigotNBTTagCompoundWrapper.wrap(null);
+        }
+
+        @Override
+        public void setTag(NBTTagCompoundWrapper tag) {
+
+        }
+
+        @Override
+        public ItemStackWrapper cloneItemStack() {
+            return new RecipeItemStackWrapper(itemstack.clone());
+        }
+
+        @Override
+        public int hashCode() {
+            return itemstack != null ? itemstack.hashCode() : 0;
+        }
+
+        @Override
+        public boolean equals(Object otherObj) {
+            if (!(otherObj instanceof RecipeItemStackWrapper)) {
+                return false;
+            }
+            RecipeItemStackWrapper other = (RecipeItemStackWrapper) otherObj;
+            return Objects.equals(itemstack, other.itemstack);
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(itemstack);
         }
     }
 }
