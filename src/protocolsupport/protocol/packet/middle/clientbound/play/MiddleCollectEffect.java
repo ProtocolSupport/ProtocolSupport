cb4b68fce044970e6af3b0153e19c5aa5bedf5c2
Raino <rainoboy97@gmail.com>
2017-10-05 01:00:34 +0200

    Fix clients below 1.9 hearing pickup sounds globally

--- src/protocolsupport/protocol/packet/middle/clientbound/play/MiddleCollectEffect.java
+++ src/protocolsupport/protocol/packet/middle/clientbound/play/MiddleCollectEffect.java
@@ -1,15 +1,15 @@
 package protocolsupport.protocol.packet.middle.clientbound.play;
 
-import java.util.concurrent.ThreadLocalRandom;
-
-import org.bukkit.Sound;
-import org.bukkit.entity.Player;
-
 import io.netty.buffer.ByteBuf;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.packet.middle.ClientBoundMiddlePacket;
 import protocolsupport.protocol.serializer.VarNumberSerializer;
 import protocolsupport.protocol.utils.types.NetworkEntity;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public abstract class MiddleCollectEffect extends ClientBoundMiddlePacket {
 
@@ -29,18 +29,19 @@ public abstract class MiddleCollectEffect extends ClientBoundMiddlePacket {
 		if (connection.getVersion().isBefore(ProtocolVersion.MINECRAFT_1_9)) {
 			Player player = connection.getPlayer();
 			NetworkEntity entity = cache.getWatchedEntity(entityId);
-			if ((entity != null) && (player != null)) {
+			if ((entity != null && entity.getUUID() != null) && (player != null)) {
+				Entity worldEntity = player.getServer().getEntity(entity.getUUID());
 				switch (entity.getType()) {
 					case ITEM: {
 						player.playSound(
-							player.getLocation(), Sound.ENTITY_ITEM_PICKUP,
+							worldEntity.getLocation(), Sound.ENTITY_ITEM_PICKUP,
 							0.2F, (((ThreadLocalRandom.current().nextFloat() - ThreadLocalRandom.current().nextFloat()) * 0.7F) + 1.0F) * 2.0F
 						);
 						break;
 					}
 					case EXP_ORB: {
 						player.playSound(
-							player.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP,
+							worldEntity.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP,
 							0.2F, (((ThreadLocalRandom.current().nextFloat() - ThreadLocalRandom.current().nextFloat()) * 0.7F) + 1.0F) * 2.0F
 						);
 						break;
