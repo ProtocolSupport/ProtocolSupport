329b00fe22b0419e81aeccbf7aaa63d1dfc49f8e
7kasper <7kasper@users.noreply.github.com>
2017-10-25 22:11:35 +0200

    > I think there is a missing ID in PEPacketIDs

--- src/protocolsupport/protocol/typeremapper/pe/PEPacketIDs.java
+++ src/protocolsupport/protocol/typeremapper/pe/PEPacketIDs.java
@@ -7,6 +7,7 @@ public class PEPacketIDs {
 	public static final int DISCONNECT = 5;
 	public static final int RESOURCE_PACK = 6;
 	public static final int RESOURCE_STACK = 7;
+	public static final int RESOURCE_RESPONSE = 8;
 	public static final int CHAT = 9;
 	public static final int UPDATE_TIME = 10;
 	public static final int START_GAME = 11;
