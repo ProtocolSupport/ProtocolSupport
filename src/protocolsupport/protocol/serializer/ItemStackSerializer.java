8bb22dd019bbbbdc498cd4b3b486f9b6b5074dac
Leonardo Malaman <leonardomalaman@gmail.com>
2017-10-26 20:28:01 -0200

    Add "revert remap" when reading the ItemStack from the client, fixes creative inventory items for PE clients

--- src/protocolsupport/protocol/serializer/ItemStackSerializer.java
+++ src/protocolsupport/protocol/serializer/ItemStackSerializer.java
@@ -20,6 +20,7 @@ import protocolsupport.api.ProtocolType;
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.api.events.ItemStackWriteEvent;
 import protocolsupport.protocol.typeremapper.itemstack.ItemStackRemapper;
+import protocolsupport.protocol.typeremapper.pe.PEDataValues;
 import protocolsupport.protocol.utils.NBTTagCompoundSerializer;
 import protocolsupport.utils.IntTuple;
 import protocolsupport.zplatform.ServerPlatform;
@@ -42,8 +43,16 @@ public class ItemStackSerializer {
 					return ItemStackWrapper.NULL;
 				}
 				int amountdata = VarNumberSerializer.readSVarInt(from);
+				int data = (amountdata >> 8) & 0xFFFF;
 				itemstack.setAmount(amountdata & 0x7F);
-				itemstack.setData((amountdata >> 8) & 0xFFFF);
+				itemstack.setData(data);
+				IntTuple itemAndData = PEDataValues.PE_ITEM_ID.getRemap(type, data);
+				if (itemAndData != null) {
+					itemstack.setTypeId(itemAndData.getI1());
+					if (itemAndData.getI2() != -1) {
+						itemstack.setData(itemAndData.getI2());
+					}
+				}
 				itemstack.setTag(readTag(from, false, version));
 				//TODO: Read the rest properly..
 				from.readByte();
