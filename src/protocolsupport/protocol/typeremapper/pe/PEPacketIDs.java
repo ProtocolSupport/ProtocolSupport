efe36a1e67727cdb3ebba24671fc644ffa973403
Leonardo Malaman <leonardomalaman@gmail.com>
2017-10-30 21:13:36 -0200

    Uncomment "Transfer" packet ID

--- src/protocolsupport/protocol/typeremapper/pe/PEPacketIDs.java
+++ src/protocolsupport/protocol/typeremapper/pe/PEPacketIDs.java
@@ -74,7 +74,7 @@ public class PEPacketIDs {
 	public static final int RESOURCE_INFO = 82;
 	public static final int RESOURCE_DATA = 83;
 	public static final int RESOURCE_REQUEST = 84;
-	//public static final int TRANSFER = 85;
+	public static final int TRANSFER = 85;
 	public static final int PLAY_SOUND = 86;
 	//public static final int STOP_SOUND = 87;
 	public static final int SET_TITLE = 88;
