97d17ffee88f1e7676dc6d9ae88aa85f59fa4087
7kasper <kasper77077@gmail.com>
2017-11-26 12:41:17 +0100

    Make other players able to switch vehicle.
    PE can only do it once? Hmmpff I ain't bothered RN.

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/SetPassengers.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/SetPassengers.java
@@ -22,7 +22,6 @@ public class SetPassengers extends MiddleSetPassengers {
 
 	public static final int UNLINK = 0;
 	public static final int LINK = 1;
-	public static final int PASSENGER = 2;
 
 	private final TIntObjectHashMap<TIntHashSet> passengers = new TIntObjectHashMap<>();
 
@@ -37,36 +36,22 @@ public class SetPassengers extends MiddleSetPassengers {
 				prevPassengersIds = new TIntHashSet();
 			}
 			TIntHashSet newPassengersIds = new TIntHashSet(passengersIds);
-			prevPassengersIds.forEach(new TIntProcedure() {
-				@Override
-				public boolean execute(int passengerId) {
-					if (!newPassengersIds.contains(passengerId)) {
-						NetworkEntity passenger = cache.getWatchedEntity(passengerId);
-						if(passenger != null) {
-							//Also update meta.
-							passenger.getDataCache().riderInfo = null;
-							packets.add(EntityMetadata.createFaux(passenger, cache.getLocale(), version));
-							packets.add(create(version, vehicleId, passengerId, UNLINK));
-							if(cache.isSelf(passengerId)) {
-								packets.add(create(version, vehicleId, 0, UNLINK));
-							}
-						}
-					}
-					return true;
-				}
-			});
 			for (int passengerId : passengersIds) {
 				NetworkEntity passenger = cache.getWatchedEntity(passengerId);
 				if (passenger != null) {
 					//MOJANG.... WHYYYYY?!
-					if(vehicle.isOfType(NetworkEntityType.PIG)) {
+					if (vehicle.isOfType(NetworkEntityType.PIG)) {
 						packets.add(EntitySetAttributes.create(version, vehicleId, EntitySetAttributes.createAttribute("minecraft:horse.jump_strength", 0.432084373616155))); 
 					}
 					
 					DataCache data = passenger.getDataCache();
+					if (data.riderInfo != null) {
+						//In case we are jumping from vehicle to vehicle.
+						packets.addAll(link(version, data.riderInfo.getVehicleId(), passengerId, UNLINK));
+					}
 					data.riderInfo = PocketData.getPocketEntityData(vehicle.getType()).getRiderInfo();
-					if(data.riderInfo != null) {
-						if(passenger.getType() == NetworkEntityType.PLAYER) {
+					if (data.riderInfo != null) {
+						if (passenger.getType() == NetworkEntityType.PLAYER) {
 							float vehicleSize = PEMetaProviderSPI.getProvider().getEntitySize(vehicle) * vehicle.getDataCache().sizeModifier;
 							data.riderInfo.setPosition(data.riderInfo.getPosition().multiply(new Vector(vehicleSize, vehicleSize, vehicleSize)));
 						} else {
@@ -75,13 +60,24 @@ public class SetPassengers extends MiddleSetPassengers {
 						data.riderInfo.setVehicleId(vehicleId);
 					}
 					packets.add(EntityMetadata.createFaux(passenger, cache.getLocale(), version));
-
-					packets.add(create(version, vehicleId, passengerId, LINK));
-					if(cache.isSelf(passengerId)) {
-						packets.add(create(version, vehicleId, 0, LINK));
-					}
+					packets.addAll(link(version, vehicleId, passengerId, LINK));
 				}
 			}
+			prevPassengersIds.forEach(new TIntProcedure() {
+				@Override
+				public boolean execute(int passengerId) {
+					if (!newPassengersIds.contains(passengerId)) {
+						NetworkEntity passenger = cache.getWatchedEntity(passengerId);
+						if (passenger != null) {
+							//Also update meta.
+							passenger.getDataCache().riderInfo = null;
+							packets.add(EntityMetadata.createFaux(passenger, cache.getLocale(), version));
+							packets.addAll(link(version, vehicleId, passengerId, UNLINK));
+						}
+					}
+					return true;
+				}
+			});
 			passengers.put(vehicleId, newPassengersIds);
 		}
 		return packets;
@@ -93,6 +89,15 @@ public class SetPassengers extends MiddleSetPassengers {
 		VarNumberSerializer.writeSVarLong(serializer, passengerId);
 		serializer.writeByte(action);
 		return serializer;
+	}
+	
+	public RecyclableArrayList<ClientBoundPacketData> link(ProtocolVersion version, int vehicleId, int passengerId, int action) {
+		RecyclableArrayList<ClientBoundPacketData> packets = RecyclableArrayList.create();
+		packets.add(create(version, vehicleId, passengerId, action));
+		if(cache.isSelf(passengerId)) {
+			packets.add(create(version, vehicleId, 0, action));
+		}
+		return packets;
 	}
 
 }
