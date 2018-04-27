0877d67916c62b3de2116de1cfec88f91aaec351
7kasper <kasper77077@gmail.com>
2018-04-27 21:39:12 +0200

    Fixed survival fly.

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/PlayerAbilities.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/PlayerAbilities.java
@@ -10,6 +10,7 @@ public class PlayerAbilities extends MiddlePlayerAbilities {
 
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData() {
+		System.out.println("FLYLYLYL: " + ((flags & flagOffsetCanFly) == flagOffsetCanFly) + " :F");
 		cache.getAttributesCache().updatePEFlying((flags & flagOffsetCanFly) == flagOffsetCanFly, (flags & flagOffsetIsFlying) == flagOffsetIsFlying);
 		return RecyclableSingletonList.create(PEAdventureSettings.createPacket(cache));
 	}
