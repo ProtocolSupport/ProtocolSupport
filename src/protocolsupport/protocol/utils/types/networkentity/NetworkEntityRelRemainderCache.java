f8fcd3b4310d5a5defbf9e6bec6a116f43843baf
entry <entrypoint_@naver.com>
2018-05-18 19:54:14 +0900

    Remove header comment

--- src/protocolsupport/protocol/utils/types/networkentity/NetworkEntityRelRemainderCache.java
+++ src/protocolsupport/protocol/utils/types/networkentity/NetworkEntityRelRemainderCache.java
@@ -2,9 +2,6 @@ package protocolsupport.protocol.utils.types.networkentity;
 
 import protocolsupport.protocol.utils.types.Position;
 
-/**
- * Created by JunHyeong Lim on 2018-05-18
- */
 public class NetworkEntityRelRemainderCache extends Position {
 	public NetworkEntityRelRemainderCache(int x, int y, int z) {
 		super(x, y, z);
