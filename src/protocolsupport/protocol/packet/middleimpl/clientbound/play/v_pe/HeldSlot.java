07e3a0f8d0f26a65a55a75b0a28ecb5e1fea553b
caoli5288 <caoli5288@gmail.com>
2018-06-29 11:21:40 +0800

    Update heldslot packet. old impl not work since 1.4

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/HeldSlot.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/HeldSlot.java
@@ -2,6 +2,8 @@ package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_pe;
 
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleHeldSlot;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
+import protocolsupport.protocol.serializer.VarNumberSerializer;
+import protocolsupport.protocol.typeremapper.pe.PEPacketIDs;
 import protocolsupport.utils.recyclable.RecyclableCollection;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
@@ -10,12 +12,19 @@ public class HeldSlot extends MiddleHeldSlot {
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData() {
 		cache.getPEInventoryCache().setSelectedSlot(slot);
-		return RecyclableSingletonList.create(EntityEquipment.createUpdateHand(connection.getVersion(), 
-			cache.getAttributesCache().getLocale(),
-			cache.getWatchedEntityCache().getSelfPlayerEntityId(), 
-			cache.getWatchedEntityCache().getSelfPlayer().getDataCache().getHand(), 
-			slot, true
-		));
+		return RecyclableSingletonList.create(create(slot));
+	}
+
+	public static ClientBoundPacketData create(int slot) {
+		return create(slot, 0, true);
+	}
+
+	public static ClientBoundPacketData create(int slot, int containerType, boolean select) {
+		ClientBoundPacketData data = ClientBoundPacketData.create(PEPacketIDs.PLAYER_HOTBAR);
+		VarNumberSerializer.writeVarInt(data, slot);
+		data.writeByte(containerType);
+		data.writeBoolean(select);
+		return data;
 	}
 
 }
