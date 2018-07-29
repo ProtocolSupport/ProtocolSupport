18773cb832916484eda1c84adddcd753e69c9d26
Shevchik <Shev4ik.den@gmail.com>
2018-07-30 01:48:13 +0300

    Transform bungeecord channel

--- src/protocolsupport/protocol/typeremapper/legacy/LegacyCustomPayloadChannelName.java
+++ src/protocolsupport/protocol/typeremapper/legacy/LegacyCustomPayloadChannelName.java
@@ -10,6 +10,7 @@ public class LegacyCustomPayloadChannelName {
 	public static final String MODERN_UNREGISTER = "minecraft:unregister";
 	public static final String MODERN_TRADER_LIST = "minecraft:trader_list";
 	public static final String MODERN_BRAND = "minecraft:brand";
+	public static final String MODERN_BUNGEECORD = "bungeecord:main";
 
 	public static final String LEGACY_REGISTER = "REGISTER";
 	public static final String LEGACY_UNREGISTER = "UNREGISTER";
@@ -21,6 +22,7 @@ public class LegacyCustomPayloadChannelName {
 	public static final String LEGACY_SET_BEACON = "MC|Beacon";
 	public static final String LEGACY_NAME_ITEM = "MC|ItemName";
 	public static final String LEGACY_PICK_ITEM = "MC|PickItem";
+	public static final String LEGACY_BUNGEECORD = "BungeeCord";
 
 	public static String toPre13(String modernName) {
 		switch (modernName) {
@@ -39,6 +41,9 @@ public class LegacyCustomPayloadChannelName {
 			case MODERN_UNREGISTER: {
 				return LEGACY_UNREGISTER;
 			}
+			case MODERN_BUNGEECORD: {
+				return LEGACY_BUNGEECORD;
+			}
 			default: {
 				return modernName;
 			}
@@ -56,13 +61,16 @@ public class LegacyCustomPayloadChannelName {
 			case "MC|Brand": {
 				return MODERN_BRAND;
 			}
+			case LEGACY_BUNGEECORD: {
+				return MODERN_BUNGEECORD;
+			}
 			default: {
 				return fixPre13(legacyName);
 			}
 		}
 	}
 
-	private static final Pattern invalid_key_pattern = Pattern.compile("[^a-z0-9._-]+");
+	protected static final Pattern invalid_key_pattern = Pattern.compile("[^a-z0-9._-]+");
 
 	@SuppressWarnings("deprecation")
 	public static String fixPre13(String legacyName) {
