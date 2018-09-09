e8e573a8d55da4161bedb53488c181d08336b779
Shevchik <Shev4ik.den@gmail.com>
2018-09-09 06:23:20 +0300

    Use heap bytebufs for pipeline operations.
    The compression and encryption pipeline elements require java byte array
    to operate. To avoid native <-> java memory copy all the time we use
    heap bytebuf and try to extract byte array from them where possible, and
    operate on it directly (passing to deflater/infater/cipher).
    Also where possible we now preallocate large enough bytebuf to fit a
    message to avoid internal reallocations on data write.

--- src/protocolsupport/utils/netty/Decompressor.java
+++ src/protocolsupport/utils/netty/Decompressor.java
@@ -3,6 +3,7 @@ package protocolsupport.utils.netty;
 import java.util.zip.DataFormatException;
 import java.util.zip.Inflater;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.util.Recycler;
 import io.netty.util.Recycler.Handle;
 
@@ -19,18 +20,26 @@ public class Decompressor {
 		return recycler.get();
 	}
 
-	private final Inflater inflater = new Inflater();
-	private final Handle<Decompressor> handle;
+	protected final Inflater inflater = new Inflater();
+	protected final Handle<Decompressor> handle;
 	protected Decompressor(Handle<Decompressor> handle) {
 		this.handle = handle;
 	}
 
-	public byte[] decompress(byte[] input, int uncompressedlength) throws DataFormatException {
-		inflater.setInput(input);
+	public byte[] decompress(byte[] input, int offset, int length, int uncompressedlength) throws DataFormatException {
+		inflater.setInput(input, offset, length);
 		byte[] uncompressed = new byte[uncompressedlength];
 		inflater.inflate(uncompressed);
 		inflater.reset();
 		return uncompressed;
+	}
+
+	protected final ReusableWriteHeapBuffer writerBuffer = new ReusableWriteHeapBuffer();
+
+	public void decompressTo(ByteBuf to, byte[] input, int offset, int length, int uncompressedlength) throws Exception {
+		inflater.setInput(input, offset, length);
+		writerBuffer.writeTo(to, uncompressedlength, inflater::inflate);
+		inflater.reset();
 	}
 
 	public void recycle() {
@@ -40,7 +49,7 @@ public class Decompressor {
 	public static byte[] decompressStatic(byte[] input, int uncompressedlength) throws DataFormatException {
 		Decompressor decompressor = create();
 		try {
-			return decompressor.decompress(input, uncompressedlength);
+			return decompressor.decompress(input, 0, input.length, uncompressedlength);
 		} finally {
 			decompressor.recycle();
 		}
