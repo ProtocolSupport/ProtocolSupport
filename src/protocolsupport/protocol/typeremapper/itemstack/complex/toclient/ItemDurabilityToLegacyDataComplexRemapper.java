64ab29864f3a15e0be1905d3b4b1ffff4989fdbc
7kasper <kasper77077@gmail.com>
2018-10-08 21:45:41 +0200

    Improve getting of nbt numbers in code from PR.

--- src/protocolsupport/protocol/typeremapper/itemstack/complex/toclient/ItemDurabilityToLegacyDataComplexRemapper.java
+++ src/protocolsupport/protocol/typeremapper/itemstack/complex/toclient/ItemDurabilityToLegacyDataComplexRemapper.java
@@ -3,13 +3,15 @@ package protocolsupport.protocol.typeremapper.itemstack.complex.toclient;
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.typeremapper.itemstack.complex.ItemStackComplexRemapper;
 import protocolsupport.protocol.utils.types.NetworkItemStack;
+import protocolsupport.protocol.utils.types.nbt.NBTNumber;
 
 public class ItemDurabilityToLegacyDataComplexRemapper implements ItemStackComplexRemapper {
 
 	@Override
 	public NetworkItemStack remap(ProtocolVersion version, String locale, NetworkItemStack itemstack) {
-		if (itemstack.getNBT() != null && itemstack.getNBT().getTag("Damage") != null) {
-			itemstack.setLegacyData(itemstack.getNBT().getNumberTag("Damage").getAsInt());
+		NBTNumber damage;
+		if (itemstack.getNBT() != null && (damage = itemstack.getNBT().getNumberTag("Damage")) != null) {
+			itemstack.setLegacyData(damage.getAsInt());
 		}
 		return itemstack;
 	}
