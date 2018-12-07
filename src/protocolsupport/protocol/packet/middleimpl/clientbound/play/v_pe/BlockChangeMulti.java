b2099f8e43058d6b775b5e338d42498dcc586732
7kasper <kasper77077@gmail.com>
2018-12-07 16:45:57 +0100

    Fix tile packet code thingy.

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/BlockChangeMulti.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/BlockChangeMulti.java
@@ -1,16 +1,11 @@
 package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_pe;
 
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
-import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.ConnectionImpl;
-import protocolsupport.protocol.packet.ClientBoundPacket;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleBlockChangeMulti;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
-import protocolsupport.protocol.serializer.ItemStackSerializer;
-import protocolsupport.protocol.serializer.PositionSerializer;
 import protocolsupport.protocol.typeremapper.basic.TileEntityRemapper;
 import protocolsupport.protocol.utils.types.Position;
-import protocolsupport.protocol.utils.types.TileEntity;
 import protocolsupport.utils.recyclable.RecyclableArrayList;
 import protocolsupport.utils.recyclable.RecyclableCollection;
 
@@ -21,14 +16,6 @@ public class BlockChangeMulti extends MiddleBlockChangeMulti {
 	}
 
 	protected final TileEntityRemapper tileremapper = TileEntityRemapper.getRemapper(connection.getVersion());
-
-	private static ClientBoundPacketData createPacket(ProtocolVersion version, TileEntity tile) {
-		ClientBoundPacketData serializer = ClientBoundPacketData.create(ClientBoundPacket.PLAY_UPDATE_TILE_ID);
-		PositionSerializer.writePosition(serializer, tile.getPosition());
-		serializer.writeByte(tile.getType().getNetworkId());
-		ItemStackSerializer.writeTag(serializer, version, tile.getNBT());
-		return serializer;
-	}
 
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData() {
@@ -42,7 +29,7 @@ public class BlockChangeMulti extends MiddleBlockChangeMulti {
 				tilestates.remove(record.localCoord);
 			}
 			if (tileremapper.usedToBeTile(record.id)) {
-				packets.add(createPacket(connection.getVersion(),
+				packets.add(BlockTileUpdate.create(connection.getVersion(),
 					tileremapper.getLegacyTileFromBlock(Position.fromLocal(chunk, record.localCoord), record.id)
 				));
 			}
