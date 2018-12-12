fe8f9c8136e72a0ea7031d0f1dcc3c61da115fdb
Colin Godsey <crgodsey@gmail.com>
2018-12-11 17:30:48 -0700

    fix

--- src/protocolsupport/protocol/pipeline/initial/InitialPacketDecoder.java
+++ src/protocolsupport/protocol/pipeline/initial/InitialPacketDecoder.java
@@ -79,7 +79,7 @@ public class InitialPacketDecoder extends SimpleChannelInboundHandler<ByteBuf> {
 		pipelineBuilders.put(ProtocolVersion.MINECRAFT_1_4_7, new protocolsupport.protocol.pipeline.version.v_1_4.PipeLineBuilder());
 		pipelineBuilders.put(ProtocolVersion.MINECRAFT_LEGACY, new protocolsupport.protocol.pipeline.version.v_l.PipeLineBuilder());
 		IPipeLineBuilder builderpe = new protocolsupport.protocol.pipeline.version.v_pe.PipeLineBuilder();
-		pipelineBuilders.put(ProtocolVersion.MINECRAFT_FUTURE, builderpe);
+		pipelineBuilders.put(ProtocolVersion.MINECRAFT_PE_FUTURE, builderpe);
 		pipelineBuilders.put(ProtocolVersion.MINECRAFT_PE_1_8, builderpe);
 		pipelineBuilders.put(ProtocolVersion.MINECRAFT_PE_1_7, builderpe);
 		pipelineBuilders.put(ProtocolVersion.MINECRAFT_PE_LEGACY, builderpe);
