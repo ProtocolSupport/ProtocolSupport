01d08670ece24ac353026a74b1df6bc8c0c7866f
Colin Godsey <crgodsey@gmail.com>
2018-12-23 08:52:24 -0600

    cleanup

--- src/protocolsupport/protocol/typeremapper/itemstack/complex/ItemStackComplexRemapper.java
+++ src/protocolsupport/protocol/typeremapper/itemstack/complex/ItemStackComplexRemapper.java
@@ -5,8 +5,7 @@ import protocolsupport.protocol.utils.types.NetworkItemStack;
 
 @FunctionalInterface
 public interface ItemStackComplexRemapper {
-
-	//TODO: All of the item remappers return a NetworkItemStack, but never make a new NetworkItemStack. they're always mutated
+	
 	public NetworkItemStack remap(ProtocolVersion version, String locale, NetworkItemStack itemstack);
 
 }
