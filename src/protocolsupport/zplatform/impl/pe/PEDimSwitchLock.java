49103ccf5a5b786148c7c921f1c6ff41ad77e69d
Colin Godsey <crgodsey@gmail.com>
2019-04-12 07:08:01 -0600

    cast down to queue

--- src/protocolsupport/zplatform/impl/pe/PEDimSwitchLock.java
+++ src/protocolsupport/zplatform/impl/pe/PEDimSwitchLock.java
@@ -4,15 +4,15 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPromise;
-
 import io.netty.util.ReferenceCountUtil;
+
 import protocolsupport.ProtocolSupport;
 import protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe.PlayerAction;
 import protocolsupport.protocol.pipeline.version.v_pe.PEPacketDecoder;
 import protocolsupport.protocol.typeremapper.pe.PEPacketIDs;
 
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
 
 //lock outbound packet stream until we get a dim switch ack
 public class PEDimSwitchLock extends ChannelDuplexHandler {
@@ -21,22 +21,22 @@ public class PEDimSwitchLock extends ChannelDuplexHandler {
 
 	protected static int MAX_QUEUE_SIZE = 4096;
 
-	protected final Deque<ByteBuf> deque = new ArrayDeque<>(32);
+	protected final Queue<ByteBuf> queue = new ArrayDeque<>(32);
 	protected boolean isLocked = false;
 
 	@Override
 	public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
 		super.channelUnregistered(ctx);
-		deque.forEach(ReferenceCountUtil::safeRelease);
-		deque.clear();
+		queue.forEach(ReferenceCountUtil::safeRelease);
+		queue.clear();
 	}
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
 		if (msg instanceof ByteBuf && isLocked && PlayerAction.isDimSwitchAck((ByteBuf) msg)) {
 			isLocked = false;
-			while (!deque.isEmpty() && !isLocked) {
-				write(ctx, deque.removeFirst(), ctx.voidPromise());
+			while (!queue.isEmpty() && !isLocked) {
+				write(ctx, queue.remove(), ctx.voidPromise());
 			}
 			ctx.flush();
 		}
@@ -47,9 +47,9 @@ public class PEDimSwitchLock extends ChannelDuplexHandler {
 	public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
 		if (msg instanceof ByteBuf) {
 			if (isLocked) {
-				deque.addLast((ByteBuf) msg);
+				queue.add((ByteBuf) msg);
 				promise.trySuccess();
-				if (deque.size() > MAX_QUEUE_SIZE) {
+				if (queue.size() > MAX_QUEUE_SIZE) {
 					ProtocolSupport.logWarning("PEDimSwitchLock: queue got too large, closing connection.");
 					ctx.channel().close();
 				}
