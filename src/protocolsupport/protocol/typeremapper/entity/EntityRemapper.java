19dae2615deb8db9744a8045aa79086ecabb68d3
Shevchik <Shev4ik.den@gmail.com>
2019-05-05 17:58:12 +0300

    Implement use bed for 1.9+

--- src/protocolsupport/protocol/typeremapper/entity/EntityRemapper.java
+++ src/protocolsupport/protocol/typeremapper/entity/EntityRemapper.java
@@ -57,6 +57,10 @@ public class EntityRemapper {
 		}
 	}
 
+	public ArrayMap<DataWatcherObject<?>> getOriginalMetadata() {
+		return originalMetadata;
+	}
+
 	public NetworkEntityType getRemappedEntityType() {
 		return remappedEntityType;
 	}
