4a63703d6707e827257c8f266a307f0ce15f9d12
Shevchik <Shev4ik.den@gmail.com>
2019-05-06 00:28:00 +0300

    Fix use bed

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_4_5_6_7/EntityMetadata.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_4_5_6_7/EntityMetadata.java
@@ -1,10 +1,19 @@
 package protocolsupport.protocol.packet.middleimpl.clientbound.play.v_4_5_6_7;
 
+import java.util.Optional;
+
+import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.ConnectionImpl;
 import protocolsupport.protocol.packet.ClientBoundPacket;
 import protocolsupport.protocol.packet.middle.clientbound.play.MiddleEntityMetadata;
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
 import protocolsupport.protocol.serializer.DataWatcherSerializer;
+import protocolsupport.protocol.serializer.PositionSerializer;
+import protocolsupport.protocol.utils.datawatcher.DataWatcherObjectIndex;
+import protocolsupport.protocol.utils.datawatcher.objects.DataWatcherObjectOptionalPosition;
+import protocolsupport.protocol.utils.networkentity.NetworkEntityType;
+import protocolsupport.protocol.utils.types.Position;
+import protocolsupport.utils.recyclable.RecyclableArrayList;
 import protocolsupport.utils.recyclable.RecyclableCollection;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
@@ -16,10 +25,32 @@ public class EntityMetadata extends MiddleEntityMetadata {
 
 	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData() {
-		ClientBoundPacketData serializer = ClientBoundPacketData.create(ClientBoundPacket.PLAY_ENTITY_METADATA_ID);
-		serializer.writeInt(entityId);
-		DataWatcherSerializer.writeLegacyData(serializer, version, cache.getAttributesCache().getLocale(), entityRemapper.getRemappedMetadata());
-		return RecyclableSingletonList.create(serializer);
+		ClientBoundPacketData entitymetadata = ClientBoundPacketData.create(ClientBoundPacket.PLAY_ENTITY_METADATA_ID);
+		entitymetadata.writeInt(entityId);
+		DataWatcherSerializer.writeLegacyData(entitymetadata, version, cache.getAttributesCache().getLocale(), entityRemapper.getRemappedMetadata());
+
+		if (entity.getType() == NetworkEntityType.PLAYER) {
+			Optional<DataWatcherObjectOptionalPosition> bedpositionObject = DataWatcherObjectIndex.EntityLiving.BED_LOCATION.getValue(entityRemapper.getOriginalMetadata());
+			if (bedpositionObject.isPresent()) {
+				Position bedposition = bedpositionObject.get().getValue();
+				if (bedposition != null) {
+					RecyclableArrayList<ClientBoundPacketData> packets = RecyclableArrayList.create();
+					packets.add(entitymetadata);
+
+					ClientBoundPacketData usebed = ClientBoundPacketData.create(ClientBoundPacket.LEGACY_PLAY_USE_BED_ID);
+					usebed.writeInt(entityId);
+					if (version.isBefore(ProtocolVersion.MINECRAFT_1_7_5)) {
+						usebed.writeByte(0);
+					}
+					PositionSerializer.writeLegacyPositionB(usebed, bedposition);
+					packets.add(usebed);
+
+					return packets;
+				}
+			}
+		}
+
+		return RecyclableSingletonList.create(entitymetadata);
 	}
 
 }
