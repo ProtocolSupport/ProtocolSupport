b609a06deacc87011e79e452ed73291610bd403c
Colin Godsey <crgodsey@gmail.com>
2019-05-08 15:46:25 -0600

    Fix plugin message format
    Apparently it *is* the varint version

--- src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_pe/CustomPayload.java
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_pe/CustomPayload.java
@@ -1,10 +1,11 @@
 package protocolsupport.protocol.packet.middleimpl.serverbound.play.v_pe;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 
 import protocolsupport.protocol.ConnectionImpl;
 import protocolsupport.protocol.packet.middle.serverbound.play.MiddleCustomPayload;
-import protocolsupport.protocol.serializer.MiscSerializer;
+import protocolsupport.protocol.serializer.ArraySerializer;
 import protocolsupport.protocol.serializer.StringSerializer;
 
 public class CustomPayload extends MiddleCustomPayload {
@@ -16,7 +17,7 @@ public class CustomPayload extends MiddleCustomPayload {
 	@Override
 	public void readFromClientData(ByteBuf clientdata) {
 		tag = StringSerializer.readString(clientdata, connection.getVersion(), 20);
-		data = MiscSerializer.readAllBytesSlice(clientdata);
+		data = Unpooled.wrappedBuffer(ArraySerializer.readVarIntByteArray(clientdata));
 	}
 
 }
