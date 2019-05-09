c7e94a120aa8ed7d1c39db438308a70888e81c6b
Shevchik <Shev4ik.den@gmail.com>
2019-05-09 17:50:54 +0300

    Use generated entity mappings

--- src/protocolsupport/protocol/typeremapper/entity/FlatteningEntityId.java
+++ src/protocolsupport/protocol/typeremapper/entity/FlatteningEntityId.java
@@ -1,16 +1,10 @@
 package protocolsupport.protocol.typeremapper.entity;
 
-import java.text.MessageFormat;
-import java.util.Arrays;
-import java.util.Map.Entry;
-
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import protocolsupport.api.ProtocolVersion;
 import protocolsupport.protocol.typeremapper.utils.RemappingRegistry.IdRemappingRegistry;
 import protocolsupport.protocol.typeremapper.utils.RemappingTable.ArrayBasedIdRemappingTable;
-import protocolsupport.protocol.types.networkentity.NetworkEntityType;
 import protocolsupport.protocol.utils.MappingsData;
 import protocolsupport.utils.JsonUtils;
 import protocolsupport.utils.ResourceUtils;
@@ -19,7 +13,6 @@ import protocolsupportbuildprocessor.Preload;
 @Preload
 public class FlatteningEntityId {
 
-	//TODO: convert to networkentitytype -> id
 	public static final IdRemappingRegistry<ArrayBasedIdRemappingTable> REGISTRY = new IdRemappingRegistry<ArrayBasedIdRemappingTable>() {
 		@Override
 		protected ArrayBasedIdRemappingTable createTable() {
@@ -27,23 +20,15 @@ public class FlatteningEntityId {
 		}
 	};
 
-	protected static void load(ProtocolVersion version) {
-		JsonObject itemsData = ResourceUtils.getAsJson(MappingsData.getFlatteningResoucePath(version, "entityl.json"));
-		if (itemsData != null) {
-			ArrayBasedIdRemappingTable table = REGISTRY.getTable(version);
-			for (Entry<String, JsonElement> entry : itemsData.entrySet()) {
-				int modernId = NetworkEntityType.getByRegistrySTypeId(entry.getKey()).getNetworkTypeId();
-				if (modernId == -1) {
-					throw new IllegalStateException(MessageFormat.format("Entity type {0} doesn''t exist", entry.getKey()));
-				}
-				int legacyId = JsonUtils.getInt(entry.getValue().getAsJsonObject(), "protocol_id");
-				table.setRemap(modernId, legacyId);
+	static {
+		JsonObject rootObject = ResourceUtils.getAsJson(MappingsData.getResourcePath("flatteningentityl.json"));
+		for (String versionString : rootObject.keySet()) {
+			JsonObject entriesObject = rootObject.get(versionString).getAsJsonObject();
+			ArrayBasedIdRemappingTable table = REGISTRY.getTable(ProtocolVersion.valueOf(versionString));
+			for (String entityidString : entriesObject.keySet()) {
+				table.setRemap(Integer.parseInt(entityidString), JsonUtils.getInt(entriesObject, entityidString));
 			}
 		}
-	}
-
-	static {
-		Arrays.stream(ProtocolVersion.getAllSupported()).forEach(FlatteningEntityId::load);
 	}
 
 }
