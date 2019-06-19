cc04f6eb6d447bc12aae197509beeaad875b936a
Shevchik <Shev4ik.den@gmail.com>
2019-06-19 05:23:11 +0300

    Remove entity tracker injector for now

--- src/protocolsupport/zplatform/impl/spigot/entitytracker/SpigotEntityTrackerEntryInjectorMap.java
+++ src/protocolsupport/zplatform/impl/spigot/entitytracker/SpigotEntityTrackerEntryInjectorMap.java
@@ -1,60 +1,60 @@
-package protocolsupport.zplatform.impl.spigot.entitytracker;
-
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.util.logging.Level;
-
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
-import net.minecraft.server.v1_14_R1.Entity;
-import net.minecraft.server.v1_14_R1.EntityTrackerEntry;
-import net.minecraft.server.v1_14_R1.EntityTypes;
-import net.minecraft.server.v1_14_R1.PlayerChunkMap.EntityTracker;
-import net.minecraft.server.v1_14_R1.WorldServer;
-import protocolsupport.ProtocolSupport;
-import protocolsupport.utils.ReflectionUtils;
-import protocolsupportbuildprocessor.Preload;
-
-@Preload
-public class SpigotEntityTrackerEntryInjectorMap extends Int2ObjectOpenHashMap<EntityTracker> {
-
-	private static final MethodHandle setTrackerEntryField = createSetTrackerEntryFieldMH();
-	private static final MethodHandle getEntityField = createGetEntityFieldMH();
-
-	private static final MethodHandle createSetTrackerEntryFieldMH() {
-		try {
-			return MethodHandles.lookup().unreflectSetter(ReflectionUtils.getField(EntityTracker.class, "trackerEntry"));
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException("Unable to create set tracker entry field method handle", e);
-		}
-	}
-
-	private static final MethodHandle createGetEntityFieldMH() {
-		try {
-			return MethodHandles.lookup().unreflectGetter(ReflectionUtils.getField(EntityTracker.class, "tracker"));
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException("Unable to create set tracker entry field method handle", e);
-		}
-	}
-
-	private static final long serialVersionUID = 1L;
-
-	@Override
-	public EntityTracker put(int k, EntityTracker v) {
-		try {
-			Entity entity = (Entity) getEntityField.invokeExact(v);
-			EntityTypes<?> entitytypes = entity.getEntityType();
-			EntityTrackerEntry entry = new SpigotEntityTrackerEntry(
-				(WorldServer) entity.world, entity,
-				entitytypes.getUpdateInterval(),
-				entitytypes.isDeltaTracking(),
-				v::broadcast,
-				v.trackedPlayers
-			);
-			setTrackerEntryField.invokeExact(v, entry);
-		} catch (Throwable e) {
-			ProtocolSupport.getInstance().getLogger().log(Level.SEVERE, e, () -> "Failed to inject entity tracker instance");
-		}
-		return super.put(k, v);
-	}
-
-}
+//package protocolsupport.zplatform.impl.spigot.entitytracker;
+//
+//import java.lang.invoke.MethodHandle;
+//import java.lang.invoke.MethodHandles;
+//import java.util.logging.Level;
+//
+//import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
+//import net.minecraft.server.v1_14_R1.Entity;
+//import net.minecraft.server.v1_14_R1.EntityTrackerEntry;
+//import net.minecraft.server.v1_14_R1.EntityTypes;
+//import net.minecraft.server.v1_14_R1.PlayerChunkMap.EntityTracker;
+//import net.minecraft.server.v1_14_R1.WorldServer;
+//import protocolsupport.ProtocolSupport;
+//import protocolsupport.utils.ReflectionUtils;
+//import protocolsupportbuildprocessor.Preload;
+//
+//@Preload
+//public class SpigotEntityTrackerEntryInjectorMap extends Int2ObjectOpenHashMap<EntityTracker> {
+//
+//	private static final MethodHandle setTrackerEntryField = createSetTrackerEntryFieldMH();
+//	private static final MethodHandle getEntityField = createGetEntityFieldMH();
+//
+//	private static final MethodHandle createSetTrackerEntryFieldMH() {
+//		try {
+//			return MethodHandles.lookup().unreflectSetter(ReflectionUtils.getField(EntityTracker.class, "trackerEntry"));
+//		} catch (IllegalAccessException e) {
+//			throw new RuntimeException("Unable to create set tracker entry field method handle", e);
+//		}
+//	}
+//
+//	private static final MethodHandle createGetEntityFieldMH() {
+//		try {
+//			return MethodHandles.lookup().unreflectGetter(ReflectionUtils.getField(EntityTracker.class, "tracker"));
+//		} catch (IllegalAccessException e) {
+//			throw new RuntimeException("Unable to create set tracker entry field method handle", e);
+//		}
+//	}
+//
+//	private static final long serialVersionUID = 1L;
+//
+//	@Override
+//	public EntityTracker put(int k, EntityTracker v) {
+//		try {
+//			Entity entity = (Entity) getEntityField.invokeExact(v);
+//			EntityTypes<?> entitytypes = entity.getEntityType();
+//			EntityTrackerEntry entry = new SpigotEntityTrackerEntry(
+//				(WorldServer) entity.world, entity,
+//				entitytypes.getUpdateInterval(),
+//				entitytypes.isDeltaTracking(),
+//				v::broadcast,
+//				v.trackedPlayers
+//			);
+//			setTrackerEntryField.invokeExact(v, entry);
+//		} catch (Throwable e) {
+//			ProtocolSupport.getInstance().getLogger().log(Level.SEVERE, e, () -> "Failed to inject entity tracker instance");
+//		}
+//		return super.put(k, v);
+//	}
+//
+//}
--- src/protocolsupport/zplatform/impl/spigot/injector/SpigotEntityTrackerInjector.java
+++ src/protocolsupport/zplatform/impl/spigot/injector/SpigotEntityTrackerInjector.java
@@ -1,27 +1,19 @@
 package protocolsupport.zplatform.impl.spigot.injector;
 
-import java.util.logging.Level;
-
-import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.world.WorldInitEvent;
-
-import net.minecraft.server.v1_14_R1.PlayerChunkMap;
-import protocolsupport.ProtocolSupport;
-import protocolsupport.utils.ReflectionUtils;
-import protocolsupport.zplatform.impl.spigot.entitytracker.SpigotEntityTrackerEntryInjectorMap;
 
 public class SpigotEntityTrackerInjector implements Listener {
 
 	@EventHandler
 	public void onWorldInit(WorldInitEvent event) {
-		PlayerChunkMap chunkmap = ((CraftWorld) event.getWorld()).getHandle().getChunkProvider().playerChunkMap;
-		try {
-			ReflectionUtils.getField(PlayerChunkMap.class, "trackedEntities").set(chunkmap, new SpigotEntityTrackerEntryInjectorMap());
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			ProtocolSupport.getInstance().getLogger().log(Level.SEVERE, e, () -> "Failed to inject entity tracker injector");
-		}
+//		PlayerChunkMap chunkmap = ((CraftWorld) event.getWorld()).getHandle().getChunkProvider().playerChunkMap;
+//		try {
+//			ReflectionUtils.getField(PlayerChunkMap.class, "trackedEntities").set(chunkmap, new SpigotEntityTrackerEntryInjectorMap());
+//		} catch (IllegalArgumentException | IllegalAccessException e) {
+//			ProtocolSupport.getInstance().getLogger().log(Level.SEVERE, e, () -> "Failed to inject entity tracker injector");
+//		}
 	}
 
 }
