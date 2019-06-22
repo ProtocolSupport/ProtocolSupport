0a8bf3a6fdcd81ee8b50fed779fe0c3da032f7ce
Shevchik <Shev4ik.den@gmail.com>
2019-06-22 10:06:37 +0300

    Fix firework entity metadata remap

--- src/protocolsupport/protocol/typeremapper/entity/metadata/types/object/FireworkEntityMetadataRemapper.java
+++ src/protocolsupport/protocol/typeremapper/entity/metadata/types/object/FireworkEntityMetadataRemapper.java
@@ -1,8 +1,12 @@
 package protocolsupport.protocol.typeremapper.entity.metadata.types.object;
 
 import protocolsupport.protocol.typeremapper.entity.metadata.types.base.BaseEntityMetadataRemapper;
+import protocolsupport.protocol.typeremapper.entity.metadata.value.IndexValueRemapper;
 import protocolsupport.protocol.typeremapper.entity.metadata.value.IndexValueRemapperNoOp;
+import protocolsupport.protocol.types.networkentity.metadata.NetworkEntityMetadataObject;
 import protocolsupport.protocol.types.networkentity.metadata.NetworkEntityMetadataObjectIndex;
+import protocolsupport.protocol.types.networkentity.metadata.objects.NetworkEntityMetadataObjectOptionalVarInt;
+import protocolsupport.protocol.types.networkentity.metadata.objects.NetworkEntityMetadataObjectVarInt;
 import protocolsupport.protocol.utils.ProtocolVersionsHelper;
 
 public class FireworkEntityMetadataRemapper extends BaseEntityMetadataRemapper {
@@ -14,7 +18,12 @@ public class FireworkEntityMetadataRemapper extends BaseEntityMetadataRemapper {
 		addRemap(new IndexValueRemapperNoOp(NetworkEntityMetadataObjectIndex.Firework.ITEM, 8), ProtocolVersionsHelper.BEFORE_1_9);
 
 		addRemap(new IndexValueRemapperNoOp(NetworkEntityMetadataObjectIndex.Firework.USER, 8), ProtocolVersionsHelper.UP_1_14);
-		addRemap(new IndexValueRemapperNoOp(NetworkEntityMetadataObjectIndex.Firework.USER, 7), ProtocolVersionsHelper.RANGE__1_11_1__1_13_2);
+		addRemap(new IndexValueRemapper<NetworkEntityMetadataObjectOptionalVarInt>(NetworkEntityMetadataObjectIndex.Firework.USER, 7) {
+			@Override
+			public NetworkEntityMetadataObject<?> remapValue(NetworkEntityMetadataObjectOptionalVarInt object) {
+				return new NetworkEntityMetadataObjectVarInt(object.getValue() != null ? object.getValue() : 0);
+			}
+		}, ProtocolVersionsHelper.RANGE__1_11_1__1_13_2);
 	}
 
 }
