0eed65b079cc3158d2e3b0855376a30285e208f4
Colin Godsey <crgodsey@gmail.com>
2019-07-12 08:29:13 -0600

    view center cleanup

--- src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/SetViewCenter.java
+++ src/protocolsupport/protocol/packet/middleimpl/clientbound/play/v_pe/SetViewCenter.java
@@ -7,19 +7,28 @@ import protocolsupport.protocol.packet.middle.clientbound.play.MiddleSetViewCent
 import protocolsupport.protocol.packet.middleimpl.ClientBoundPacketData;
 import protocolsupport.protocol.serializer.VarNumberSerializer;
 import protocolsupport.protocol.typeremapper.pe.PEPacketIDs;
+import protocolsupport.protocol.types.Position;
 import protocolsupport.utils.recyclable.RecyclableCollection;
 import protocolsupport.utils.recyclable.RecyclableSingletonList;
 
 public class SetViewCenter extends MiddleSetViewCenter {
+
+	protected Position pos;
 
 	public SetViewCenter(ConnectionImpl connection) {
 		super(connection);
 	}
 
 	@Override
+	public boolean postFromServerRead() {
+		pos = new Position(chunk.getX() * 16, 64, chunk.getZ() * 16);
+		cache.getMovementCache().setChunkPublisherPosition(pos);
+		return true;
+	}
+
+	@Override
 	public RecyclableCollection<ClientBoundPacketData> toData() {
-		return RecyclableSingletonList.create(createChunkPublisherUpdate(
-			chunk.getX() * 16, 64, chunk.getZ() * 16));
+		return RecyclableSingletonList.create(createChunkPublisherUpdate(pos));
 	}
 
 	public static void writeChunkPublisherUpdate(ByteBuf out, int x, int y, int z) {
@@ -27,6 +36,10 @@ public class SetViewCenter extends MiddleSetViewCenter {
 		VarNumberSerializer.writeVarInt(out, y);
 		VarNumberSerializer.writeSVarInt(out, z);
 		VarNumberSerializer.writeVarInt(out, 512); //radius, gets clamped by client
+	}
+
+	public static ClientBoundPacketData createChunkPublisherUpdate(Position pos) {
+		return createChunkPublisherUpdate(pos.getX(), pos.getY(), pos.getZ());
 	}
 
 	public static ClientBoundPacketData createChunkPublisherUpdate(int x, int y, int z) {
