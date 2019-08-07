5f022bce89428d4b526fc564f9a2c9575e7d4f17
Shevchik <Shev4ik.den@gmail.com>
2019-08-08 01:02:26 +0300

    Use bit utils createBitMask

--- src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_6_7/SteerVehicle.java
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_6_7/SteerVehicle.java
@@ -11,12 +11,13 @@ public class SteerVehicle extends MiddleSteerVehicle {
 		super(connection);
 	}
 
+	protected static final int[] flags_mask = new int[] {FLAGS_BIT_JUMPING, FLAGS_BIT_UNMOUNT};
+
 	@Override
 	public void readFromClientData(ByteBuf clientdata) {
 		sideForce = clientdata.readFloat();
 		forwardForce = clientdata.readFloat();
-		BitUtils.setIBit(flags, FLAGS_BIT_JUMPING, clientdata.readByte());
-		BitUtils.setIBit(flags, FLAGS_BIT_UNMOUNT, clientdata.readByte());
+		flags = BitUtils.createIBitMaskFromBits(flags_mask, new int[] {clientdata.readByte(), clientdata.readByte()});
 	}
 
 }
