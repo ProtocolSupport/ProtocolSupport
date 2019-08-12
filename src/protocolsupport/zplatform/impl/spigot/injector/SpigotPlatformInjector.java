9121e72fe5337c4f415d6306363ff84cd21daebc
Shevchik <Shev4ik.den@gmail.com>
2019-08-12 18:52:52 +0300

    Move into SpigotPlatformInjector

--- src/protocolsupport/zplatform/impl/spigot/injector/SpigotPlatformInjector.java
+++ src/protocolsupport/zplatform/impl/spigot/injector/SpigotPlatformInjector.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 
 import protocolsupport.ProtocolSupport;
 import protocolsupport.zplatform.PlatformInjector;
+import protocolsupport.zplatform.impl.spigot.block.SpigotBlocksBoundsAdjust;
 import protocolsupport.zplatform.impl.spigot.injector.network.SpigotNettyInjector;
 
 public class SpigotPlatformInjector implements PlatformInjector {
@@ -11,7 +12,7 @@ public class SpigotPlatformInjector implements PlatformInjector {
 	@Override
 	public void onLoad() {
 		try {
-			SpigotServerInjector.inject();
+			SpigotBlocksBoundsAdjust.inject();
 			SpigotNettyInjector.inject();
 		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
 			throw new RuntimeException("Error while injecting", e);
