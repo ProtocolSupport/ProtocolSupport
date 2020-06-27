a0ab26f899d7e5920777883e889f66fca377e6da
Lennart ten Wolde <lenny.devmail@gmail.com>
2020-06-27 21:03:44 +0200

    Cancel special-case block place packets from 1.8->1.9

--- src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_4_5_6_7/BlockPlace.java
+++ src/protocolsupport/protocol/packet/middleimpl/serverbound/play/v_4_5_6_7/BlockPlace.java
@@ -2,6 +2,7 @@ package protocolsupport.protocol.packet.middleimpl.serverbound.play.v_4_5_6_7;
 
 import io.netty.buffer.ByteBuf;
 import protocolsupport.protocol.ConnectionImpl;
+import protocolsupport.protocol.packet.middle.CancelMiddlePacketException;
 import protocolsupport.protocol.packet.middle.serverbound.play.MiddleBlockPlace;
 import protocolsupport.protocol.serializer.ItemStackSerializer;
 import protocolsupport.protocol.serializer.PositionSerializer;
@@ -20,9 +21,19 @@ public class BlockPlace extends MiddleBlockPlace {
 		PositionSerializer.readLegacyPositionBTo(clientdata, position);
 		face = clientdata.readByte();
 		ItemStackSerializer.readItemStack(clientdata, version);
-		cX = clientdata.readUnsignedByte() / 16.0F;
-		cY = clientdata.readUnsignedByte() / 16.0F;
-		cZ = clientdata.readUnsignedByte() / 16.0F;
+
+		byte cursorX = clientdata.readByte();
+		byte cursorY = clientdata.readByte();
+		byte cursorZ = clientdata.readByte();
+
+		// Cancel "special case" packets because they are not sent on 1.9
+		if(cursorX == -1 && cursorY == -1 && cursorZ == -1 && face == -1) {
+			throw CancelMiddlePacketException.INSTANCE;
+		}
+
+		cX = Byte.toUnsignedInt(cursorX) / 16.0F;
+		cY = Byte.toUnsignedInt(cursorY) / 16.0F;
+		cZ = Byte.toUnsignedInt(cursorZ) / 16.0F;
 	}
 
 }
