1f834da42dfc1171e5a76aa573e61821677708e9
Shevchik <Shev4ik.den@gmail.com>
2020-07-02 22:30:14 +0300

    Forgot to commit new particle files

---
+++ src/protocolsupport/protocol/types/particle/types/ParticleSoul.java
@@ -0,0 +1,14 @@
+package protocolsupport.protocol.types.particle.types;
+
+import protocolsupport.protocol.types.particle.Particle;
+
+public class ParticleSoul extends Particle {
+
+	public ParticleSoul() {
+	}
+
+	public ParticleSoul(float offsetX, float offsetY, float offsetZ, float data, int count) {
+		super(offsetX, offsetY, offsetZ, data, count);
+	}
+
+}
---
+++ src/protocolsupport/protocol/types/particle/types/ParticleSoulFlame.java
@@ -0,0 +1,14 @@
+package protocolsupport.protocol.types.particle.types;
+
+import protocolsupport.protocol.types.particle.Particle;
+
+public class ParticleSoulFlame extends Particle {
+
+	public ParticleSoulFlame() {
+	}
+
+	public ParticleSoulFlame(float offsetX, float offsetY, float offsetZ, float data, int count) {
+		super(offsetX, offsetY, offsetZ, data, count);
+	}
+
+}
