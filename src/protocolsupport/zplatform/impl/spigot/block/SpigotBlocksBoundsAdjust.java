ca4fd291f4eef9e2c1d465fd69cad3c1297ec689
Shevchik <Shev4ik.den@gmail.com>
2021-04-13 04:01:12 +0300

    Do not interrupt load on block bounds update failure
    The protocol translation layer will still work without block bounds
    changes

--- src/protocolsupport/zplatform/impl/spigot/block/SpigotBlocksBoundsAdjust.java
+++ src/protocolsupport/zplatform/impl/spigot/block/SpigotBlocksBoundsAdjust.java
@@ -12,19 +12,24 @@ import net.minecraft.server.v1_16_R3.BlockWaterLily;
 import net.minecraft.server.v1_16_R3.Blocks;
 import net.minecraft.server.v1_16_R3.VoxelShape;
 import net.minecraft.server.v1_16_R3.VoxelShapes;
+import protocolsupport.ProtocolSupport;
 import protocolsupport.utils.ReflectionUtils;
 
 public class SpigotBlocksBoundsAdjust {
 
 	public static void inject() {
-		ReflectionUtils.setStaticFinalField(BlockWaterLily.class, "a", VoxelShapes.create(0.0625D, 0.0, 0.0625D, 0.9375D, 0.015625D, 0.9375D));
+		try {
+			ReflectionUtils.setStaticFinalField(BlockWaterLily.class, "a", VoxelShapes.create(0.0625D, 0.0, 0.0625D, 0.9375D, 0.015625D, 0.9375D));
 
-		ReflectionUtils.setStaticFinalField(BlockLadder.class, "c", VoxelShapes.create(0.0D, 0.0D, 0.0D, 0.125D, 1.0D, 1.0D));
-		ReflectionUtils.setStaticFinalField(BlockLadder.class, "d", VoxelShapes.create(0.875D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D));
-		ReflectionUtils.setStaticFinalField(BlockLadder.class, "e", VoxelShapes.create(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 0.125D));
-		ReflectionUtils.setStaticFinalField(BlockLadder.class, "f", VoxelShapes.create(0.0D, 0.0D, 0.875D, 1.0D, 1.0D, 1.0D));
+			ReflectionUtils.setStaticFinalField(BlockLadder.class, "c", VoxelShapes.create(0.0D, 0.0D, 0.0D, 0.125D, 1.0D, 1.0D));
+			ReflectionUtils.setStaticFinalField(BlockLadder.class, "d", VoxelShapes.create(0.875D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D));
+			ReflectionUtils.setStaticFinalField(BlockLadder.class, "e", VoxelShapes.create(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 0.125D));
+			ReflectionUtils.setStaticFinalField(BlockLadder.class, "f", VoxelShapes.create(0.0D, 0.0D, 0.875D, 1.0D, 1.0D, 1.0D));
 
-		ReflectionUtils.setStaticFinalField(BlockCarpet.class, "a", VoxelShapes.create(0, 0, 0, 1.0D, 0.00001, 1.0D));
+			ReflectionUtils.setStaticFinalField(BlockCarpet.class, "a", VoxelShapes.create(0, 0, 0, 1.0D, 0.00001, 1.0D));
+		} catch (Throwable t) {
+			ProtocolSupport.logError("Unable to inject custom block bounds", t);
+		}
 
 		{
 			Field shapesField1 = ReflectionUtils.getField(BlockTall.class, "g");
