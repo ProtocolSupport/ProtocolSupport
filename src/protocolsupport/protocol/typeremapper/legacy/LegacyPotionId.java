66b494a8dd1e3e17bdc6a6d6b5535fe1013e973f
Shevchik <Shev4ik.den@gmail.com>
2021-04-14 19:09:14 +0300

    Improve legacy potion remapping
    Map modern id to legacy type id and then add throwable/drinkable bits.
    Check throwable bit for isThrowable check
    Use only type id bits for legeacy id -> modern id mapping.

--- src/protocolsupport/protocol/typeremapper/legacy/LegacyPotionId.java
+++ src/protocolsupport/protocol/typeremapper/legacy/LegacyPotionId.java
@@ -4,71 +4,101 @@ import org.bukkit.NamespacedKey;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
+import protocolsupport.protocol.utils.NamespacedKeyUtils;
+import protocolsupport.utils.BitUtils;
 import protocolsupportbuildprocessor.Preload;
 
 @Preload
 public class LegacyPotionId {
 
-	private static final Object2IntOpenHashMap<String> toLegacyId = new Object2IntOpenHashMap<>();
-	private static final Int2ObjectOpenHashMap<String> fromLegacyId = new Int2ObjectOpenHashMap<>();
+	private static final Object2IntOpenHashMap<String> toLegacyTypeId = new Object2IntOpenHashMap<>();
+	private static final Int2ObjectOpenHashMap<String> fromLegacyTypeId = new Int2ObjectOpenHashMap<>();
+	private static final int bitmaskStrong = BitUtils.createIBitMaskFromBit(5);
+	private static final int bitmaskExtended = BitUtils.createIBitMaskFromBit(6);
+	private static final int bitmaskType = BitUtils.createIBitMaskFromBits(new int[] {0, 1, 2, 3}) | bitmaskStrong | bitmaskExtended;
+	private static final int bitmaskDrinkable = BitUtils.createIBitMaskFromBit(13);
+	private static final int bitmaskThrowable = BitUtils.createIBitMaskFromBit(14);
+
+	private static void register(String name, int id, int... additionalIds) {
+		NamespacedKey key = NamespacedKeyUtils.fromString(name);
+		toLegacyTypeId.put(key.getKey(), id);
+		toLegacyTypeId.put(key.toString(), id);
+		fromLegacyTypeId.put(id, key.toString());
+		for (int aId : additionalIds) {
+			fromLegacyTypeId.put(aId, key.toString());
+		}
+	}
+
+	private static int makeStrong(int baseId) {
+		return baseId | bitmaskStrong;
+	}
+
+	private static int makeExtended(int baseId) {
+		return baseId | bitmaskExtended;
+	}
+
 	static {
 		register("water", 0);
 		register("awkward", 16);
-		register("thick", 32);
-		register("mundane", 64);
-		register("night_vision", 8230);
-		register("long_night_vision", 8262);
-        register("invisibility", 8238);
-        register("long_invisibility", 8270);
-        register("leaping", 8203);
-        register("long_leaping", 8267);
-        register("strong_leaping", 8235);
-        register("fire_resistance", 8227);
-        register("long_fire_resistance", 8259);
-        register("swiftness", 8194);
-        register("long_swiftness", 8258);
-        register("strong_swiftness", 8226);
-        register("slowness", 8234);
-        register("long_slowness", 8266);
-        register("water_breathing", 8237);
-        register("long_water_breathing", 8269);
-        register("healing", 8261);
-        register("strong_healing", 8229);
-        register("harming", 8268);
-        register("strong_harming", 8236);
-        register("poison", 8196);
-        register("long_poison", 8260);
-        register("strong_poison", 8228);
-        register("regeneration", 8193);
-        register("long_regeneration", 8257);
-        register("strong_regeneration", 8225);
-        register("strength", 8201);
-        register("long_strength", 8265);
-        register("strong_strength", 8233);
-        register("weakness", 8232);
-        register("long_weakness", 8264);
-	}
+		register("thick", makeStrong(0));
+		register("mundane", makeExtended(0));
 
-	private static void register(String name, int id) {
-		toLegacyId.put(name, id);
-		toLegacyId.put(NamespacedKey.minecraft(name).toString(), id);
-		fromLegacyId.put(id, NamespacedKey.minecraft(name).toString());
+		register("regeneration", 1);
+		register("long_regeneration", makeExtended(1));
+		register("strong_regeneration", makeStrong(1));
+
+		register("swiftness", 2);
+		register("long_swiftness", makeExtended(2));
+		register("strong_swiftness", makeStrong(2));
+
+		register("fire_resistance", makeStrong(3), 3);
+		register("long_fire_resistance", makeExtended(3));
+
+		register("poison", 4);
+		register("long_poison", makeExtended(4));
+		register("strong_poison", makeStrong(4));
+
+		register("healing", makeStrong(5), 5);
+		register("strong_healing", makeExtended(5));
+
+		register("night_vision", makeStrong(6), 6);
+		register("long_night_vision", makeExtended(6));
+
+		register("weakness", makeStrong(8), 8);
+		register("long_weakness", makeExtended(8));
+
+		register("strength", 9);
+		register("long_strength", makeExtended(9));
+		register("strong_strength", makeStrong(9));
+
+		register("slowness", makeStrong(10), 10);
+		register("long_slowness", makeExtended(10));
+
+		register("leaping", 11);
+		register("long_leaping", makeExtended(11));
+		register("strong_leaping", makeStrong(11));
+
+		register("harming", makeExtended(12), 12);
+		register("strong_harming", makeStrong(12));
+
+		register("water_breathing", makeStrong(13), 13);
+		register("long_water_breathing", makeExtended(13));
+
+		register("invisibility", makeStrong(14), 14);
+		register("long_invisibility", makeExtended(14));
 	}
 
 	public static int toLegacyId(String nbttag, boolean isThrowable) {
-		int value = toLegacyId.getInt(nbttag);
-		return isThrowable ? value + 8192 : value;
+		int value = toLegacyTypeId.getInt(nbttag);
+		return isThrowable ? value | bitmaskThrowable : value | bitmaskDrinkable;
 	}
 
 	public static boolean isThrowable(int legacyId) {
-		return legacyId > 16384;
+		return (legacyId & bitmaskThrowable) != 0;
 	}
 
 	public static String fromLegacyId(int legacyId) {
-		if (isThrowable(legacyId)) {
-			legacyId -= 8192;
-		}
-		return fromLegacyId.get(legacyId);
+		return fromLegacyTypeId.get(legacyId & bitmaskType);
 	}
 
 }
