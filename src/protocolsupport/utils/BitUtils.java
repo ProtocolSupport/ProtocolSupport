e9aaf71d266f5cda51c133e9c5b363a9d52bf090
Shevchik <Shev4ik.den@gmail.com>
2021-04-14 19:04:03 +0300

    More BitUtils shorcut methods

--- src/protocolsupport/utils/BitUtils.java
+++ src/protocolsupport/utils/BitUtils.java
@@ -11,8 +11,18 @@ public class BitUtils {
 	}
 
 	public static int setIBit(int bitmask, int bitpos, int value) {
-		bitmask &= ~(1L << bitpos);
+		bitmask &= ~(1 << bitpos);
 		bitmask |= (value << bitpos);
+		return bitmask;
+	}
+
+	public static int setIBit(int bitmask, int bitpos) {
+		bitmask |= (1 << bitpos);
+		return bitmask;
+	}
+
+	public static int clearIBit(int bitmask, int bitpos) {
+		bitmask &= ~(1 << bitpos);
 		return bitmask;
 	}
 
@@ -30,12 +40,30 @@ public class BitUtils {
 		return bitmask;
 	}
 
+	public static long setLBit(long bitmask, int bitpos) {
+		bitmask |= (1L << bitpos);
+		return bitmask;
+	}
+
+	public static long clearLBit(long bitmask, int bitpos) {
+		bitmask &= ~(1L << bitpos);
+		return bitmask;
+	}
+
 	public static int createIBitMaskFromBit(int bitpos, int value) {
 		return value << bitpos;
 	}
 
+	public static int createIBitMaskFromBit(int bitpos) {
+		return 1 << bitpos;
+	}
+
 	public static long createLBitMaskFromBit(int bitpos, long value) {
 		return value << bitpos;
+	}
+
+	public static long createLBitMaskFromBit(int bitpos) {
+		return 1L << bitpos;
 	}
 
 	public static int createIBitMaskFromBits(int[] bitpos, int[] values) {
@@ -47,11 +75,27 @@ public class BitUtils {
 		return bitmask;
 	}
 
+	public static int createIBitMaskFromBits(int[] bitpos) {
+		int bitmask = 0;
+		for (int bit : bitpos) {
+			bitmask |= (1 << bit);
+		}
+		return bitmask;
+	}
+
 	public static long createLBitMaskFromBits(int[] bitpos, long[] values) {
 		long bitmask = 0;
 		int length = Math.min(bitpos.length, values.length);
 		for (int i = 0; i < length; i++) {
 			bitmask |= (values[i] << bitpos[i]);
+		}
+		return bitmask;
+	}
+
+	public static long createLBitMaskFromBits(int[] bitpos) {
+		long bitmask = 0;
+		for (int bit : bitpos) {
+			bitmask |= (1L << bit);
 		}
 		return bitmask;
 	}
