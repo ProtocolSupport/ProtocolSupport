1be3c57be3675d4facefde8660a5495f1efa7d6d
Shevchik <Shev4ik.den@gmail.com>
2022-03-12 14:34:19 +0300

    Disable command declare for now

--- src/protocolsupport/protocol/packet/middle/base/clientbound/play/MiddleDeclareCommands.java
+++ src/protocolsupport/protocol/packet/middle/base/clientbound/play/MiddleDeclareCommands.java
@@ -2,7 +2,9 @@ package protocolsupport.protocol.packet.middle.base.clientbound.play;
 
 import io.netty.buffer.ByteBuf;
 import protocolsupport.protocol.codec.MiscDataCodec;
+import protocolsupport.protocol.packet.middle.MiddlePacketCancelException;
 import protocolsupport.protocol.packet.middle.base.clientbound.ClientBoundMiddlePacket;
+import protocolsupport.protocol.utils.ProtocolVersionsHelper;
 
 public abstract class MiddleDeclareCommands extends ClientBoundMiddlePacket {
 
@@ -16,6 +18,11 @@ public abstract class MiddleDeclareCommands extends ClientBoundMiddlePacket {
 	@Override
 	protected void decode(ByteBuf serverdata) {
 		data = MiscDataCodec.readAllBytesSlice(serverdata);
+
+		//TODO: remove after implementing
+		if (version.isBefore(ProtocolVersionsHelper.LATEST_PC)) {
+			throw MiddlePacketCancelException.INSTANCE;
+		}
 	}
 
 }
